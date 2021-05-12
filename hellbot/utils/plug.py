b23da38472a43af741935f4116cc65fe8e4f461c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-12 10:15:26 +0530

    Create plug.py

---
+++ hellbot/utils/plug.py
@@ -0,0 +1,95 @@
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import os
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
+
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from var import Var
+
+from hellbot import *
+from hellbot.helpers import *
+from hellbot.config import *
+
+
+# ENV
+ENV = bool(os.environ.get("ENV", False))
+if ENV:
+    from hellbot.config import Config
+else:
+    if os.path.exists("Config.py"):
+        from Config import Development as Config
+
+
+# load plugins
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import hellbot.utils
+
+        path = Path(f"hellbot/plugins/{shortname}.py")
+        name = "hellbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("HellBot - Successfully imported " + shortname)
+    else:
+        import hellbot.utils
+
+        path = Path(f"hellbot/plugins/{shortname}.py")
+        name = "hellbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = bot
+        mod.tgbot = bot.tgbot
+        mod.Var = Var
+        mod.command = command
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        sys.modules["uniborg.util"] = hellbot.utils
+        mod.Config = Config
+        mod.borg = bot
+        mod.hellbot = bot
+        mod.edit_or_reply = edit_or_reply
+        mod.eor = edit_or_reply
+        mod.delete_hell = delete_hell
+        # support for other userbots
+        sys.modules["userbot.utils"] = hellbot.utils
+        sys.modules["userbot"] = hellbot
+        # support for paperplaneextended
+        sys.modules["userbot.events"] = hellbot.utils
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["hellbot.plugins." + shortname] = mod
+        LOGS.info("HellBot - Successfully imported " + shortname)
+
+
+# remove plugins
+def remove_plugin(shortname):
+    try:
+        try:
+            for i in LOAD_PLUG[shortname]:
+                bot.remove_event_handler(i)
+            del LOAD_PLUG[shortname]
+
+        except BaseException:
+            name = f"hellbot.plugins.{shortname}"
+
+            for i in reversed(range(len(bot._event_builders))):
+                ev, cb = bot._event_builders[i]
+                if cb.__module__ == name:
+                    del bot._event_builders[i]
+    except BaseException:
+        raise ValueError
