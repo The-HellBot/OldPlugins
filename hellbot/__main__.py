10afd5b4f5843267ef9ce425cedac6998a8851e3
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-12 14:11:16 +0530

    Create __main__.py

---
+++ hellbot/__main__.py
@@ -0,0 +1,90 @@
+import glob
+import os
+import sys
+from pathlib import Path
+
+import telethon.utils
+from telethon import TelegramClient
+
+from hellbot import LOGS, bot
+from hellbot.config import Config
+from hellbot.utils import load_module
+
+HELL_PIC = Config.ALIVE_PIC or "https://telegra.ph/  " #soon
+
+# let's get the bot ready
+async def hell_bot(bot_token):
+    try:
+        await bot.start(bot_token)
+        bot.me = await bot.get_me()
+        bot.uid = telethon.utils.get_peer_id(bot.me)
+    except Exception as e:
+        LOGS.error(f"STRING_SESSION - {str(e)}")
+        sys.exit()
+
+
+# hellbot starter...
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    try:
+        if Config.BOT_USERNAME is not None:
+            LOGS.info("Checking Telegram Bot Username...")
+            bot.tgbot = TelegramClient(
+                "BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
+            ).start(bot_token=Config.BOT_TOKEN)
+            LOGS.info("Checking Completed. Proceeding to next step...")
+            LOGS.info("🔰 Starting HellBot 🔰")
+            bot.loop.run_until_complete(hell_bot(Config.BOT_USERNAME))
+            LOGS.info("🔥 HellBot Startup Completed 🔥")
+        else:
+            bot.start()
+    except Exception as e:
+        LOGS.error(f"BOT_TOKEN - {str(e)}")
+        sys.exit()
+
+# imports plugins...
+path = "hellbot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        try:
+            if shortname.replace(".py", "") not in Config.NO_LOAD:
+                load_module(shortname.replace(".py", ""))
+            else:
+                os.remove(Path(f"hellbot/plugins/{shortname}.py"))
+        except Exception as e:
+            os.remove(Path(f"hellbot/plugins/{shortname}.py"))
+            LOGS.info(f"{shortname} import failed due to error {e}")
+
+# let the party begin...
+LOGS.info("⚡ Your HellBot Is Now Working ⚡")
+LOGS.info(
+    "Head to @The_HellBot for Updates. Also join chat group to get help regarding to HellBot."
+)
+
+# that's life...
+async def hell_is_on():
+    try:
+        if Config.LOGGER != 0:
+            await bot.send_file(
+                Config.LOGGER,
+                HELL_PIC,
+                caption="Deployed Hêllẞø† Successfully\n\nType `.ping` or `.alive` to check! \n\nJoin [Hêllẞø† Channel](t.me/The_HellBot) for Updates & [Hêllẞø† Chat](t.me/its_fuckin_hell) for any query regarding Hêllẞø†",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+
+
+bot.loop.create_task(hell_is_on())
+
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    bot.run_until_disconnected()
+
+# hellbot
