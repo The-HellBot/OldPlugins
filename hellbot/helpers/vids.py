c2eb9f40afd0897d7c340bcd60d7132eeb0a07a6
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-13 13:39:35 +0530

    Create vids.py

---
+++ hellbot/helpers/vids.py
@@ -0,0 +1,109 @@
+import asyncio
+import os
+try:
+    pass
+except:
+    os.system("pip install colour")
+import re
+import requests
+import time
+
+from bs4 import BeautifulSoup
+import PIL.ImageOps
+from PIL import Image
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from validators.url import url
+
+
+# generate thumbnail from audio...
+async def thumb_from_audio(audio_path, output):
+    await runcmd(f"ffmpeg -i {audio_path} -filter:v scale=500:500 -an {output}")
+
+# take a frame from video
+async def take_screen_shot(video_file, output_directory, ttl):
+    # https://stackoverflow.com/a/13891070/4723940
+    out_put_file_name = output_directory + "/" + str(time.time()) + ".jpg"
+    file_genertor_command = [
+        "ffmpeg",
+        "-ss",
+        str(ttl),
+        "-i",
+        video_file,
+        "-vframes",
+        "1",
+        out_put_file_name,
+    ]
+    # width = "90"
+    process = await asyncio.create_subprocess_exec(
+        *file_genertor_command,
+        # stdout must a pipe to be accessible as process.stdout
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+    e_response = stderr.decode().strip()
+    t_response = stdout.decode().strip()
+    if os.path.lexists(out_put_file_name):
+        return out_put_file_name
+    else:
+        logger.info(e_response)
+        logger.info(t_response)
+        return None
+
+# trim vids
+async def cult_small_video(video_file, output_directory, start_time, end_time):
+    # https://stackoverflow.com/a/13891070/4723940
+    out_put_file_name = output_directory + "/" + str(round(time.time())) + ".mp4"
+    file_genertor_command = [
+        "ffmpeg",
+        "-i",
+        video_file,
+        "-ss",
+        start_time,
+        "-to",
+        end_time,
+        "-async",
+        "1",
+        "-strict",
+        "-2",
+        out_put_file_name,
+    ]
+    process = await asyncio.create_subprocess_exec(
+        *file_genertor_command,
+        # stdout must a pipe to be accessible as process.stdout
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+    e_response = stderr.decode().strip()
+    t_response = stdout.decode().strip()
+    if os.path.lexists(out_put_file_name):
+        return out_put_file_name
+    else:
+        logger.info(e_response)
+        logger.info(t_response)
+        return None
+
+# makes animated sticker to gif
+async def make_gif(event, file):
+    chat = "@tgstogifbot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            await silently_send_message(conv, "/start")
+            await event.client.send_file(chat, file)
+            response = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+            if response.text.startswith("Send me an animated sticker!"):
+                return "`This file is not supported`"
+            response = response if response.media else await conv.get_response()
+            hellresponse = response if response.media else await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+            hellfile = await event.client.download_media(hellresponse, "./temp")
+            return await unzip(hellfile)
+        except YouBlockedUserError:
+            return "Unblock @tgstogifbot"
+
+
+# hellbot
