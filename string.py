05b1dc057d097c742ce50b5d5fdda00f94340c6c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-14 14:36:31 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,21 @@
+import os
+os.system("pip install telethon")
+import telethon
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+
+okvai = input("Enter 69 to continue: ")
+if okvai == "69":
+    print("Please go to my.telegram.org and get your API Id and API Hash to proceed.")
+    APP_ID = int(input("Enter APP ID here: "))
+    API_HASH = input("Enter API HASH here: ")
+
+    with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+        print(client.session.save())
+        client.send_message("me", client.session.save())
+
+else:
+    print("Bhag jaa bhosdike")
+
+# hellbot
