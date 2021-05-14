a32c058856dc40f8b9ea1e35bfeed9500d97e913
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-14 14:11:49 +0530

    Update Config.py

--- hellbot/config/Config.py
+++ hellbot/config/Config.py
@@ -1,1 +1,13 @@
-# Ok later
+# Configs imports from here
+
+import os
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    from hell_config import Config
+else:
+    if os.path.exists("config.py"):
+        from config import Development as Config
+
+# hellbot
