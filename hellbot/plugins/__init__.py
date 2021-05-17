b1ce4a4e8586d1a5ef0e0a3b218b26a16d130c9e
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-17 08:32:31 +0530

    Update __init__.py

--- hellbot/plugins/__init__.py
+++ hellbot/plugins/__init__.py
@@ -1,1 +1,24 @@
+from hellbot import *
+from hellbot.config import Config
+from hellbot.helpers import *
+from hellbot.utils import *
 
+
+HELL_USER = Config.YOUR_NAME or "Hêll"
+ForGot10God = bot.uid
+hell_mention = f"[{HELL_USER}](tg://user?id={ForGo10God})"
+
+
+sudos = Config.SUDO_USERS
+
+if sudos:
+    is_sudo = "True"
+else:
+    is_sudo = "False"
+
+chnl_link = "https://t.me/the_hellbot"
+hell_channel = f"[†hê Hêllẞø†]({chnl_link})"
+
+# will add more soon
+
+# hellbot
