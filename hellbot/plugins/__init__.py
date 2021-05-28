75c4a964b23ba7a7a123a206c06e455c06a14f08
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-28 20:46:46 +0530

    Update __init__.py

--- hellbot/plugins/__init__.py
+++ hellbot/plugins/__init__.py
@@ -7,6 +7,7 @@ from hellbot.utils import *
 HELL_USER = Config.YOUR_NAME or "Hêll"
 ForGo10God = bot.uid
 hell_mention = f"[{HELL_USER}](tg://user?id={ForGo10God})"
+hell_logo = "./hellbot/resources/hellbot_logo.jpg"
 
 
 sudos = Config.SUDO_USERS
