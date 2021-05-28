e5e2a16585eca4b9105a3f3473b175503d6a9bd3
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-28 14:46:32 +0530

    Create _helper.py

---
+++ hellbot/plugins/_helper.py
@@ -0,0 +1,32 @@
+import asyncio
+import requests
+from telethon import functions
+
+from . import *
+
+@bot.on(hell_cmd(pattern="help ?(.*)", outgoing=True))
+async def yardim(event):
+    if event.fwd_from:
+        return
+    tgbotusername = Config.BOT_USERNAME
+    input_str = event.pattern_match.group(1)
+    if tgbotusername is not None or input_str == "text":
+        results = await event.client.inline_query(tgbotusername, "@The_HellBot)
+        await results[0].click(
+            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+        )
+        await event.delete()
+    else:
+        await eor(event, ["NO_BOT"])
+    
+        if input_str in CMD_LIST:
+          string = "Commands found in {}:\n".format(input_str)
+          for i in CMD_LIST[input_str]:
+              string += "  " + i
+              string += "\n"
+          await event.edit(string)
+        else:
+          await event.edit(input_str + " is not a valid plugin!")
+
+
+# hellbot
