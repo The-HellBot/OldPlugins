8604ebe74147b7201a7d4bb610ddb3caa8eaead2
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-28 20:47:41 +0530

    Create _core.py

---
+++ hellbot/plugins/_core.py
@@ -0,0 +1,144 @@
+import asyncio
+from datetime import datetime
+import os
+from pathlib import Path
+
+from telethon import events, functions, types
+from telethon.tl.types import InputMessagesFilterDocument
+
+from . import *
+
+
+@bot.on(hell_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
+async def send(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    thumb = hell_logo
+    input_str = event.pattern_match.group(1)
+    omk = f"**• Plugin name ≈** `{input_str}`\n**• Uploaded by ≈** {hell_mention}\n\n⚡ **[ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt](t.me/hellbot_official)** ⚡"
+    the_plugin_file = "./hellbot/plugins/{}.py".format(input_str)
+    if os.path.exists(the_plugin_file):
+        lauda = await event.client.send_file(
+            event.chat_id,
+            the_plugin_file,
+            thumb=thumb,
+            caption=omk,
+            force_document=True,
+            allow_cache=False,
+            reply_to=message_id,
+        )
+        await event.delete()
+    else:
+        await edit_or_reply(event, "File not found..... Kek")
+
+
+@bot.on(hell_cmd(pattern="install$", outgoing=True))
+@bot.on(sudo_cmd(pattern="install$", allow_sudo=True))
+async def install(event):
+    if event.fwd_from:
+        return
+    a = "__Installing.__"
+    b = 1
+    await event.edit(a)
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
+                await event.get_reply_message(),
+                "./hellbot/plugins/"  # pylint:disable=E0602
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                if shortname in CMD_LIST:
+                    string = "**Commands found in** `{}`\n".format((os.path.basename(downloaded_file_name)))
+                    for i in CMD_LIST[shortname]:
+                        string += "  •  `" + i 
+                        string += "`\n"
+                        if b == 1:
+                            a = "__Installing..__"
+                            b = 2
+                        else:
+                            a = "__Installing...__"
+                            b = 1
+                        await event.edit(a)
+                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {hell_mention}\n\n{string}\n\n        ⚡ **[ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt](t.me/hellbot_official)** ⚡", link_preview=False)
+                return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
+            else:
+                os.remove(downloaded_file_name)
+                return await event.edit(f"**Failed to Install** \n`Error`\nModule already installed or unknown format")
+        except Exception as e: 
+            await event.edit(f"**Failed to Install** \n`Error`\n{str(e)}")
+            return os.remove(downloaded_file_name)
+
+@bot.on(hell_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
+async def uninstall(kraken):
+    if kraken.fwd_from:
+        return
+    shortname = kraken.pattern_match["shortname"]
+    dir_path =f"./hellbot/plugins/{shortname}.py"
+    try:
+        remove_plugin(shortname)
+        os.remove(dir_path)
+        await kraken.edit(f"Uninstalled `{shortname}` successfully")
+    except OSError as e:
+        await kraken.edit("Error: %s : %s" % (dir_path, e.strerror))
+
+
+@bot.on(hell_cmd(pattern=r"unload (?P<shortname>\w+)$"))
+@bot.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
+async def unload(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        remove_plugin(shortname)
+        await event.edit(f"Successfully unloaded `{shortname}`")
+    except Exception as e:
+        await event.edit(
+            "Successfully unloaded {shortname}\n{}".format(
+                shortname, str(e)
+            )
+        )
+
+
+@bot.on(hell_cmd(pattern=r"load (?P<shortname>\w+)$"))
+@bot.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
+async def load(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        try:
+            remove_plugin(shortname)
+        except BaseException:
+            pass
+        load_module(shortname)
+        await event.edit(f"Successfully loaded `{shortname}`")
+    except Exception as e:
+        await event.edit(
+            f"Sorry, could not load {shortname} because of the following error.\n{str(e)}"
+        )
+
+CmdHelp("core").add_command(
+  "install", "<reply to a .py file>", "Installs the replied python file if suitable to Hêllẞø†'s codes."
+).add_command(
+  "uninstall", "<plugin name>", "Uninstalls the given plugin from Hêllẞø†. To get that again do .restart", "uninstall alive"
+).add_command(
+  "load", "<plugin name>", "Loades the unloaded plugin to your userbot", "load alive"
+).add_command(
+  "unload", "<plugin name>", "Unloads the plugin from your userbot", "unload alive"
+).add_command(
+  "send", "<file name>", "Sends the given file from your userbot server, if any.", "send alive"
+).add_command(
+  "cmds", None, "Gives out the list of modules in HellBot."
+).add_warning(
+  "Don't install unknown plugins. If you do and something happens with your data only you will be liable for that."
+).add()
+
+# hellbot
