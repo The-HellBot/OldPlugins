2300973d1ed783d08a63bd52b9d330278da3d848
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-29 09:42:57 +0530

    Create ping.py

---
+++ hellbot/plugins/ping.py
@@ -0,0 +1,24 @@
+import asyncio
+from datetime import datetime
+
+from . import *
+
+@bot.on(hell_cmd(pattern="ping$"))
+@bot.on(sudo_cmd(pattern="ping$", allow_sudo=True))
+async def pong(hell):
+    if hell.fwd_from:
+        return
+    start = datetime.now()
+    event = await eor(hell, "`·.·★ ℘ıŋɠ ★·.·´")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(
+        f"╰•★★  ℘ơŋɠ ★★•╯\n\n    ⚘  {ms}\n    ⚘  __**Oɯɳҽɾ**__ **:** {hell_mention}"
+    )
+
+
+CmdHelp("ping").add_command(
+  "ping", None, "Checks the ping speed of your Hêllẞø†"
+).add_warning(
+  "✅ Harmless Module"
+).add()
