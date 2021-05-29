70b92d2f40b860f8e016029400c6bf9b2adeaf77
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-29 13:12:40 +0530

    Create runner.py

---
+++ hellbot/helpers/runner.py
@@ -0,0 +1,31 @@
+import asyncio
+import functools
+import shlex
+from typing import Tuple
+
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+def run_sync(func, *args, **kwargs):
+    return asyncio.get_event_loop().run_in_executor(
+        None, functools.partial(func, *args, **kwargs)
+    )
+
+
+def run_async(loop, coro):
+    return asyncio.run_coroutine_threadsafe(coro, loop).result()
+
+
+# hellbot
