8bfacde20636827bbc5f91c3ccb334a92732b464
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-29 19:29:26 +0530

    Abb to aadat si h vro

--- hellbot/plugins/afk.py
+++ hellbot/plugins/afk.py
@@ -101,7 +101,7 @@ async def on_afk(event):
         last_afk_message[event.chat_id] = msg  # pylint:disable=E0602
 
 
-@bot.on(admin_cmd(pattern=r"afk (.*)", outgoing=True))  # pylint:disable=E0602
+@bot.on(hell_cmd(pattern=r"afk (.*)", outgoing=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
