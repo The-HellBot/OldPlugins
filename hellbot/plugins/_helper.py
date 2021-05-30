dde601559bc83801074d114c9841912afd25dabc
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-31 00:53:25 +0530

    Update _helper.py

--- hellbot/plugins/_helper.py
+++ hellbot/plugins/_helper.py
@@ -17,7 +17,7 @@ async def yardim(event):
         )
         await event.delete()
     else:
-        await eor(event, ["NO_BOT"])
+        await eor(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
     
         if input_str in CMD_LIST:
           string = "Commands found in {}:\n".format(input_str)
@@ -28,5 +28,33 @@ async def yardim(event):
         else:
           await event.edit(input_str + " is not a valid plugin!")
 
+@bot.on(hell_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
+async def hellbott(event):
+    if event.fwd_from:
+        return
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("**⚠️ Error !** \nNeed a module name to show plugin info.")
+    else:
+        string = ""
+        sayfa = [
+            sorted(list(CMD_HELP))[i : i + 5]
+            for i in range(0, len(sorted(list(CMD_HELP))), 5)
+        ]
+
+        for i in sayfa:
+            string += f"`▶️ `"
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await event.edit("Please Specify A Module Name Of Which You Want Info" + "\n\n" + string)
 
 # hellbot
