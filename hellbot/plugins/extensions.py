faa1e6cedde370e6e02aaf6ecff115cd672c1f4a
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-02 04:58:33 +0530

    Create extensions.py

---
+++ hellbot/plugins/extensions.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+from . import *
+
+@bot.on(hell_cmd(pattern="fext (.*)"))
+@bot.on(sudo_cmd(pattern="fext (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = "https://www.fileext.com/file-extension/{}.html"
+    input_str = event.pattern_match.group(1).lower()
+    response_api = requests.get(sample_url.format(input_str))
+    status_code = response_api.status_code
+    if status_code == 200:
+        raw_html = response_api.content
+        soup = BeautifulSoup(raw_html, "html.parser")
+        ext_details = soup.find_all("td", {"colspan": "3"})[-1].text
+        await edit_or_reply(
+            event,
+            "**File Extension**: `{}`\n**Description**: `{}`".format(
+                input_str, ext_details
+            ),
+        )
+    else:
+        await edit_or_reply(
+            event,
+            "https://www.fileext.com/ responded with {} for query: {}".format(
+                status_code, input_str
+            ),
+        )
+
+
+CmdHelp("fileext").add_command(
+  "filext", "<extension name>", "Shows you the detailed information of that extension type."
+).add_info(
+  "File Extensions."
+).add_warning(
+  "✅ Harmless Module."
+).add()
