a132108d58f3d5c0228d83cf65bda1a1d3fea019
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-01 07:51:31 +0530

    Update _core.py

--- hellbot/plugins/_core.py
+++ hellbot/plugins/_core.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import datetime
+import io
 import os
 from pathlib import Path
 
@@ -7,6 +8,39 @@ from telethon import events, functions, types
 from telethon.tl.types import InputMessagesFilterDocument
 
 from . import *
+
+
+@bot.on(hell_cmd(pattern=r"cmds"))
+@bot.on(sudo_cmd(pattern=r"cmds", allow_sudo=True))
+async def kk(event):
+    if event.fwd_from:
+        return
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    cmd = "ls hellbot/plugins"
+    thumb = hell_logo
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    _o = o.split("\n")
+    o = "\n".join(_o)
+    OUTPUT = f"List of Plugins in bot :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin {hell_grp} for help."
+    if len(OUTPUT) > 69:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "cmd_list.text"
+            hell_file = await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                thumb=thumb,
+                reply_to=reply_to_id,
+            )
+            await edit_or_reply(hell_file, f"Output Too Large. This is the file for the list of plugins in bot.\n\n**BY :-** {HELL_USER}")
+            await event.delete()
 
 
 @bot.on(hell_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
