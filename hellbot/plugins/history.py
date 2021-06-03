1d35f92c0ada75fe2915f07553f7b404e6f74d7f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-03 06:04:04 +0530

    Create history.py

---
+++ hellbot/plugins/history.py
@@ -0,0 +1,82 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from . import *
+
+@bot.on(hell_cmd(pattern="history ?(.*)"))
+@bot.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
+async def _(hellevent):
+    if hellevent.fwd_from:
+        return 
+    if not hellevent.reply_to_msg_id:
+       await eod(hellevent, "`Please Reply To A User To Get This Module Work`")
+       return
+    reply_message = await hellevent.get_reply_message() 
+    chat = "Sangmatainfo_bot"
+    victim = reply_message.sender.id
+    if reply_message.sender.bot:
+       await eod(hellevent, "Need actual users. Not Bots")
+       return
+    await eor(hellevent, "Checking...")
+    async with hellevent.client.conversation(chat) as conv:
+          try:     
+              response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response3 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await conv.send_message("/search_id {}".format(victim))
+              response1 = await response1 
+              response2 = await response2 
+              response3 = await response3 
+          except YouBlockedUserError: 
+              await eod(hellevent, "Please unblock @Sangmatainfo_bot")
+              return
+          if response1.text.startswith("No records found"):
+             await eor(hellevent, "User never changed his Username...")
+          else: 
+             await hellevent.delete()
+             await hellevent.client.send_message(hellevent.chat_id, response2.message)
+
+
+@bot.on(hell_cmd(pattern="unh ?(.*)"))
+@bot.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
+async def _(hellevent):
+    if hellevent.fwd_from:
+        return 
+    if not hellevent.reply_to_msg_id:
+       await eod(hellevent, "`Please Reply To A User To Get This Module Work`")
+       return
+    reply_message = await hellevent.get_reply_message() 
+    chat = "Sangmatainfo_bot"
+    victim = reply_message.sender.id
+    if reply_message.sender.bot:
+       await eod(hellevent, "Need actual users. Not Bots")
+       return
+    await eor(hellevent, "Checking...")
+    async with hellevent.client.conversation(chat) as conv:
+          try:     
+              response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response3 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await conv.send_message("/search_id {}".format(victim))
+              response1 = await response1 
+              response2 = await response2 
+              response3 = await response3 
+          except YouBlockedUserError: 
+              await eod(hellevent, "Please unblock @Sangmatainfo_bot")
+              return
+          if response1.text.startswith("No records found"):
+             await eor(hellevent, "User never changed his Username...")
+          else: 
+             await hellevent.delete()
+             await hellevent.client.send_message(hellevent.chat_id, response3.message)
+
+
+CmdHelp("history").add_command(
+  "history", "<reply to a user>", "Fetches the name history of replied user."
+).add_command(
+  "unh", "<reply to user>", "Fetches the Username History of replied users."
+).add_info(
+  "Telegram Name History"
+).add_warning(
+  "✅ Harmless Module."
+).add()
