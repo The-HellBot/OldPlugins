22323ac892b24a79f55556a59a6c4e05352fda10
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-03 19:49:49 +0530

    Update archiver.py

--- hellbot/plugins/archiver.py
+++ hellbot/plugins/archiver.py
@@ -18,6 +18,49 @@ extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
 if not os.path.isdir(extracted):
     os.makedirs(extracted)
 
+@bot.on(hell_cmd(pattern="zip", outgoing=True))
+@bot.on(sudo_cmd(pattern="zip", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.is_reply:
+        await eod(event, "Reply to a file to compress it. Bruh.")
+        return
+    mone = await eor(event, "Processing ...")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        try:
+            downloaded_file_name = await bot.download_media(
+                reply_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+            )
+            directory_name = downloaded_file_name
+            await edit_or_reply(event, downloaded_file_name)
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await mone.edit(str(e))
+    zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED).write(
+        directory_name
+    )
+    await bot.send_file(
+        event.chat_id,
+        directory_name + ".zip",
+        caption="**Zipped!**",
+        force_document=True,
+        allow_cache=False,
+        reply_to=event.message.id,
+    )
+    await asyncio.sleep(7)
+    await event.delete()
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+            os.remove(os.path.join(root, file))
 
 @bot.on(hell_cmd(pattern="compress"))
 @bot.on(sudo_cmd(pattern="compress", allow_sudo=True))
