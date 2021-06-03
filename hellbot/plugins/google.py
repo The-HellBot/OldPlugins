b7a968ff0d5401a0c8d8c1f393c5dafe89a93069
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-03 22:55:25 +0530

    Update google.py

--- hellbot/plugins/google.py
+++ hellbot/plugins/google.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 from shutil import rmtree
 import datetime
-
+import wikipedia
 import requests
 from bs4 import BeautifulSoup
 from search_engine_parser import GoogleSearch
@@ -17,6 +17,23 @@ def progress(current, total):
         "Downloaded {} of {}\nCompleted {}".format(
             current, total, (current / total) * 100
         )
+    )
+
+
+@bot.on(hell_cmd(pattern="wikipedia (.*)"))
+@bot.on(sudo_cmd(pattern="wikipedia (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await edit_or_reply(event, "Processing ...")
+    input_str = event.pattern_match.group(1)
+    result = ""
+    results = wikipedia.search(input_str)
+    for s in results:
+        page = wikipedia.page(s)
+        url = page.url
+        result += f"> [{s}]({url}) \n"
+    await edit_or_reply(event, "WikiPedia **Search**: {} \n\n **Result**: \n\n{}".format(input_str, result)
     )
 
 
@@ -173,6 +190,8 @@ CmdHelp("google").add_command(
   "reverse", "<reply to a sticker/pic>", "Does a reverse image search on google and provides the similar images"
 ).add_command(
   "gps", "<place>", "Gives the location of the given place/city/state."
+).add_command(
+  "wikipedia", "<query>", "Searches for the query on Wikipedia."
 ).add_info(
   "Google Search."
 ).add_warning(
