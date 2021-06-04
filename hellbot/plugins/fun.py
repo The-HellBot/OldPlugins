fe55a24599d0acc44ab692fca775b3bcadc8b81a
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-05 05:11:14 +0530

    Update fun.py

--- hellbot/plugins/fun.py
+++ hellbot/plugins/fun.py
@@ -1,7 +1,82 @@
 import random
 from random import choice
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+
 from . import *
 
+
+@bot.on(hell_cmd(pattern="slap ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="slap ?(.*)", allow_sudo=True))
+async def who(event):
+    if event.fwd_from:
+        return
+    replied_user = await get_user(event)
+    caption = await slap(replied_user, event)
+    message_id_to_reply = event.message.reply_to_msg_id
+
+    if not message_id_to_reply:
+        message_id_to_reply = None
+
+    try:
+        await edit_or_reply(event, caption)
+
+    except:
+        await edit_or_reply(event, "`Can't slap this nibba !!`")
+
+
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(GetFullUserRequest(previous_message.sender_id))
+    else:
+        user = event.pattern_match.group(1)
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+
+        except (TypeError, ValueError):
+            await edit_or_reply(event, "`I don't slap strangers !!`")
+            return None
+
+    return replied_user
+
+
+async def slap(replied_user, event):
+    user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    username = replied_user.user.username
+    if username:
+        slapped = "@{}".format(username)
+    else:
+        slapped = f"[{first_name}](tg://user?id={user_id})"
+
+    temp = random.choice(SLAP_TEMPLATES)
+    item = random.choice(ITEMS)
+    hit = random.choice(HIT)
+    throw = random.choice(THROW)
+
+    caption = temp.format(
+        user1=hell_mention, user2=slapped, item=item, hits=hit, throws=throw
+    )
+
+    return caption
 
 @bot.on(hell_cmd(pattern=f"randi$", outgoing=True))
 @bot.on(sudo_cmd(pattern=f"randi$", allow_sudo=True))
@@ -119,6 +194,8 @@ CmdHelp("fun").add_command(
   "chu", None, "Use and see"
 ).add_command(
   "noob", None, "Fuckin Noobs"
+).add_command(
+  "slap", "<reply>", "Slaps the replied user."
 ).add_info(
   "Bakchodi Hai Bass."
 ).add_warning(
