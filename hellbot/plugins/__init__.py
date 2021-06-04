fde701c63c0ff73283e072858e199912455bcdba
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-04 16:13:44 +0530

    Update __init__.py

--- hellbot/plugins/__init__.py
+++ hellbot/plugins/__init__.py
@@ -4,7 +4,7 @@ from hellbot.helpers import *
 from hellbot.utils import *
 
 
-DEVLIST = ["1748056101", "1432756163", "1746995580", "1777340882", "816517310"]
+DEVLIST = ["1748056101", "1432756163", "1746995580", "1777340882", "816517310", "1384994671"]
 HELL_USER = Config.YOUR_NAME or "Hêll"
 ForGo10God = bot.uid
 hell_mention = f"[{HELL_USER}](tg://user?id={ForGo10God})"
