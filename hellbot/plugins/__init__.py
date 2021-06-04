fe1d4b00402503a4686f970c09877fb22f1be426
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-04 11:11:44 +0530

    Update __init__.py

--- hellbot/plugins/__init__.py
+++ hellbot/plugins/__init__.py
@@ -31,6 +31,22 @@ hell_channel = f"[†hê Hêllẞø†]({chnl_link})"
 grp_link = "https://t.me/HellBot_Chat"
 hell_grp = f"[Hêllẞø† Group]({grp_link})"
 
+WELCOME_FORMAT = """**Use these fomats in your welcome note to make them attractive.**
+
+  {mention} :  To mention the user
+  {title} : To get chat name in message
+  {count} : To get group members
+  {first} : To use user first name
+  {last} : To use user last name
+  {fullname} : To use user full name
+  {userid} : To use userid
+  {username} : To use user username
+  {my_first} : To use my first name
+  {my_fullname} : To use my full name
+  {my_last} : To use my last name
+  {my_mention} : To mention myself
+  {my_username} : To use my username
+"""
 # will add more soon
 
 # hellbot
