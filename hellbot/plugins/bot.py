61d75671e23f3686dc1ebbb9ca50bd041fc2d205
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-05 05:42:56 +0530

    Update bot.py

--- hellbot/plugins/bot.py
+++ hellbot/plugins/bot.py
@@ -37,7 +37,29 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())
     result = result.stringify()
     logger.info(result)
-    await eor(event, result)
+    await eor("Config Saved In You Heroku Logs.")
+
+
+@bot.on(hell_cmd(pattern="schd ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="schd ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    ttl = 0
+    message = f"SYNTAX: `{hl}schd <time_in_seconds> - <message to send>`"
+    if input_str:
+        await event.delete()
+        if "-" in input_str:
+            ttl, message = input_str.split("-")
+        elif event.reply_to_msg_id:
+            await event.delete()
+            ttl = int(input_str)
+            message = await event.get_reply_message()
+        await asyncio.sleep(int(ttl))
+        await event.respond(message)
+    else:
+        await event.edit(message)
 
 
 @bot.on(hell_cmd(pattern="dm ?(.*)"))
