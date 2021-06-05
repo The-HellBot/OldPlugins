0a18ec31bb47ffefe490319005653a50b08e5a24
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-06 01:36:43 +0530

    Update fun2.py

--- hellbot/plugins/fun2.py
+++ hellbot/plugins/fun2.py
@@ -1,10 +1,40 @@
 import asyncio
 import random
+import requests
 import nekos
 
+from os import remove
 from telethon.tl.types import ChannelParticipantsAdmins
+from urllib import parse
 
 from . import *
+
+BASE_URL = "https://headp.at/pats/{}"
+PAT_IMAGE = "pat.jpg"
+
+
+@bot.on(hell_cmd(pattern="pat ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="pat ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    username = event.pattern_match.group(1)
+    if not username and not event.reply_to_msg_id:
+        await eod(event, "`Reply to a message or provide username`")
+        return
+
+    resp = requests.get("http://headp.at/js/pats.json")
+    pats = resp.json()
+    pat = BASE_URL.format(parse.quote(random.choice(pats)))
+    await event.delete()
+    with open(PAT_IMAGE, "wb") as f:
+        f.write(requests.get(pat).content)
+    if username:
+        await bot.send_file(event.chat_id, PAT_IMAGE, caption=username)
+    else:
+        await bot.send_file(event.chat_id, PAT_IMAGE, reply_to=event.reply_to_msg_id)
+    remove(PAT_IMAGE)
+
 
 @bot.on(hell_cmd(pattern="join$", outgoing=True))
 @bot.on(sudo_cmd(pattern="join$", allow_sudo=True))
