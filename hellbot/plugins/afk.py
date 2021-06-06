d9cf3bb97faf57bcfa8cb260a7cacd4a97394df3
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-07 01:02:23 +0530

    Update afk.py

--- hellbot/plugins/afk.py
+++ hellbot/plugins/afk.py
@@ -158,3 +158,136 @@ CmdHelp("afk").add_command(
 ).add_warning(
   "✅ Harmless Module."
 ).add()
+
+
+global USER_night
+global night_time 
+global last_night_message
+USER_night = {}
+night_time = None
+last_night_message = {}
+
+
+@bot.on(events.NewMessage(outgoing=True))
+async def set_not_night(event):
+    global USER_night 
+    global night_time 
+    global last_night_message
+    current_message = event.message.message
+    if ".night" not in current_message and "yes" in USER_night:
+        try:
+            await bot.send_message(
+                Config.LOGGER_ID,
+                f"#NIGHT \n\nNight Mode :  **TRUE**",
+            )
+        except Exception as e:
+            await bot.send_message(
+                event.chat_id,
+                "Please set `LOGGER_ID` "
+                + "for the proper functioning of night functionality "
+                + "report in {}\n\n `{}`".format(hell_grp, str(e)),
+                reply_to=event.message.id,
+                silent=True,
+            )
+        USER_night = {}
+        night_time = None
+
+
+@bot.on(hell_cmd(pattern=r"night ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    global USER_night
+    global night_time
+    global last_night_message
+    global reason
+    USER_night = {}
+    night_time = None
+    last_night_message = {}
+    reason = event.pattern_match.group(1)
+    if not USER_night:
+        last_seen_status = await bot(
+            functions.account.GetPrivacyRequest(types.InputPrivacyKeyStatusTimestamp())
+        )
+        if isinstance(last_seen_status.rules, types.PrivacyValueAllowAll):
+            night_time = datetime.datetime.now()
+        USER_night = f"yes: {reason}"
+        if reason:
+            await event.edit(f"**Bye Fellas!!** \n\nTime to sleep 😴")
+        else:
+            await event.edit(f"**Bye Fellas!!** \n\nTime to sleep 😴")
+        await asyncio.sleep(5)
+        await event.delete()
+        try:
+            await bot.send_message(
+                Config.LOGGER_ID, f"Time to sleep 😴"
+            )
+        except Exception as e:
+            logger.warn(str(e))
+
+
+@bot.on(
+    events.NewMessage(
+        incoming=True, func=lambda e: bool(e.mentioned or e.is_private)
+    )
+)
+async def on_night(event):
+    if event.fwd_from:
+        return
+    global USER_night
+    global night_time
+    global last_night_message
+    night_since = "**a while ago**"
+    current_message_text = event.message.message.lower()
+    if "night" in current_message_text:
+        # userbot's should not reply to other userbot's
+        # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
+        return False
+    if USER_night and not (await event.get_sender()).bot:
+        if night_time:
+            now = datetime.datetime.now()
+            datime_since_night = now - night_time
+            time = float(datime_since_night.seconds)
+            days = time // (24 * 3600)
+            time = time % (24 * 3600)
+            hours = time // 3600
+            time %= 3600
+            minutes = time // 60
+            time %= 60
+            seconds = time
+            if days == 1:
+                night_since = "**Yesterday**"
+            elif days > 1:
+                if days > 6:
+                    date = now + datetime.timedelta(
+                        days=-days, hours=-hours, minutes=-minutes
+                    )
+                    night_since = date.strftime("%A, %Y %B %m, %H:%I")
+                else:
+                    wday = now + datetime.timedelta(days=-days)
+                    night_since = wday.strftime("%A")
+            elif hours > 1:
+                night_since = f"`{int(hours)}h{int(minutes)}m` **ago**"
+            elif minutes > 0:
+                night_since = f"`{int(minutes)}m{int(seconds)}s` **ago**"
+            else:
+                night_since = f"`{int(seconds)}s` **ago**"
+        msg = None
+        message_to_reply = (
+            f"My Master Has Been Gone For {night_since}\nWhere He Is: **On Bed Sleeping** "
+            if reason
+            else f"I'm sleeping right now!!"
+        )
+        msg = await event.reply(message_to_reply)
+        await asyncio.sleep(5)
+        if event.chat_id in last_night_message:
+            await last_night_message[event.chat_id].delete()
+        last_night_message[event.chat_id] = msg
+
+CmdHelp("night").add_command(
+  "night", None, "Same like AFK. But fixed reason and for sleeping purpose only. Sed ;_;"
+).add_info(
+  "Good Night 🌃"
+).add_warning(
+  "✅ Harmless Module."
+).add()
