5eba5bbbc4882a0ddb03d801f7b8f215bdf15434
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-07 20:46:39 +0530

    Create memevoice.py

---
+++ hellbot/plugins/memevoice.py
@@ -0,0 +1,72 @@
+import re
+from . import *
+
+# Credits to @ForGo10God developer of HellBot.
+# This is my first plugin that I made when I released first HellBot.
+# Modified to work in groups with inline mode disabled.
+# Added error msg if no voice is found.
+# So please dont remove credit. 
+# You can use it in your repo. But dont remove these lines...
+
+@bot.on(hell_cmd(pattern="mev(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="mev(?: |$)(.*)", allow_sudo=True))
+async def nope(kraken):
+    hell = kraken.pattern_match.group(1)
+    if not hell:
+        if kraken.is_reply:
+            (await kraken.get_reply_message()).message
+        else:
+            await edit_or_reply(kraken, "`Sir please give some query to search and download it for you..!`"
+            )
+            return
+
+    troll = await bot.inline_query("TrollVoiceBot", f"{(deEmojify(hell))}")
+    if troll:
+        await kraken.delete()
+        hel_ = await troll[0].click(Config.LOGGER_ID)
+        if hel_:
+            await bot.send_file(
+                kraken.chat_id,
+                hel_,
+                caption="",
+            )
+        await hel_.delete()
+    else:
+    	await eod(kraken, "**Error 404:**  Not Found")
+    	
+@bot.on(hell_cmd(pattern="meev(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="meev(?: |$)(.*)", allow_sudo=True))
+async def nope(kraken):
+    hell = kraken.pattern_match.group(1)
+    if not hell:
+        if kraken.is_reply:
+            (await kraken.get_reply_message()).message
+        else:
+            await edit_or_reply(kraken, "`Sir please give some query to search and download it for you..!`"
+            )
+            return
+
+    troll = await bot.inline_query("Myinstantsbot", f"{(deEmojify(hell))}")
+    if troll:
+        await kraken.delete()
+        hel_ = await troll[0].click(Config.LOGGER_ID)
+        if hel_:
+            await bot.send_file(
+                kraken.chat_id,
+                hel_,
+                caption="",
+            )
+        await hel_.delete()
+    else:
+    	await eod(kraken, "**Error 404:**  Not Found")
+
+
+CmdHelp("memevoice").add_command(
+	"mev", "<query>", "Searches the given meme and sends audio if found."
+).add_command(
+	"meev", "<query>", "Same as {hl}mev"
+).add_info(
+	"Audio Memes."
+).add_warning(
+	"✅ Harmless Module."
+).add()
