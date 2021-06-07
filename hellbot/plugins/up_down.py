8e599f1c466c5bf2a0c278fd876f0c91b8523368
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-07 23:06:25 +0530

    Update up_down.py

--- hellbot/plugins/up_down.py
+++ hellbot/plugins/up_down.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import time
 import datetime
+import requests
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -12,6 +13,63 @@ from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo
 
 from . import *
+
+@bot.on(hell_cmd(pattern=r"webup ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"webup ?(.*)", allow_sudo=True))
+async def labstack(event):
+    if event.fwd_from:
+        return
+    await eor(event, "Processing...")
+    input_str = event.pattern_match.group(1)
+    reply = await event.get_reply_message()
+    if input_str:
+        filebase = input_str
+    elif reply:
+        filebase = await event.client.download_media(
+            reply.media, Config.TMP_DOWNLOAD_DIRECTORY
+        )
+    else:
+        await eod(event, "Reply to a media file or provide a directory to upload the file to labstack"
+        )
+        return
+    filesize = os.path.getsize(filebase)
+    filename = os.path.basename(filebase)
+    headers2 = {"Up-User-ID": "IZfFbjUcgoo3Ao3m"}
+    files2 = {
+        "ttl": 604800,
+        "files": [{"name": filename, "type": "", "size": filesize}],
+    }
+    r2 = requests.post(
+        "https://up.labstack.com/api/v1/links", json=files2, headers=headers2
+    )
+    r2json = json.loads(r2.text)
+
+    url = "https://up.labstack.com/api/v1/links/{}/send".format(r2json["code"])
+    max_days = 7
+    command_to_exec = [
+        "curl",
+        "-F",
+        "files=@" + filebase,
+        "-H",
+        "Transfer-Encoding: chunked",
+        "-H",
+        "Up-User-ID: IZfFbjUcgoo3Ao3m",
+        url,
+    ]
+    try:
+        logger.info(command_to_exec)
+        t_response = subprocess.check_output(command_to_exec, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as exc:
+        logger.info("Status : FAIL", exc.returncode, exc.output)
+        await eor(event, exc.output.decode("UTF-8"))
+        return
+    else:
+        logger.info(t_response)
+        t_response_arry = "https://up.labstack.com/api/v1/links/{}/receive".format(
+            r2json["code"]
+        )
+    await eor(event, t_response_arry + "\nMax Days:" + str(max_days), link_preview=False
+    )
 
 
 @bot.on(admin_cmd(pattern=r"upld_dir (.*)", outgoing=True))
@@ -365,6 +423,8 @@ CmdHelp("up_down").add_command(
   "upldir", "<path>", "Uploads all the files in directory"
 ).add_command(
   "download", "<link/filename> or reply to media", "Downloads the file to the server"
+).add_command(
+  "webup", "<reply to media>", "Makes a direct download link of the replied media for a limited time"
 ).add_info(
   "Upload & Download."
 ).add_warning(
