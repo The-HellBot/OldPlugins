756b3746e99211c492edb52747aba2170535f8a3
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-07 14:27:10 +0530

    Update mmf.py

--- hellbot/helpers/mmf.py
+++ hellbot/helpers/mmf.py
@@ -15,7 +15,7 @@ async def draw_meme_text(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "hellbot/resources/fonts/italic.ttf", int((70 / 640) * i_width)
+        "hellbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
@@ -122,7 +122,7 @@ async def draw_meme(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "hellbot/resources/fonts/italic.ttf", int((70 / 640) * i_width)
+        "hellbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
