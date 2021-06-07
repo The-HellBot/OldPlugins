5aeef96ab8384bf331ffae552aa17ac0e1fc86f7
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-07 23:18:36 +0530

    Update get_data.py

--- hellbot/plugins/get_data.py
+++ hellbot/plugins/get_data.py
@@ -1,3 +1,4 @@
+import io
 import datetime
 import string
 
@@ -98,10 +99,41 @@ async def _(event):
     yaml_text = yaml_format(msg)
     await edit_or_reply(event, yaml_text, parse_mode=parse_pre)
 
+@bot.on(hell_cmd(pattern="json$", outgoing=True))
+@bot.on(admin_cmd(pattern="json$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    the_real_message = None
+    reply_to_id = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        the_real_message = previous_message.stringify()
+        reply_to_id = event.reply_to_msg_id
+    else:
+        the_real_message = event.stringify()
+        reply_to_id = event.message.id
+    if len(the_real_message) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(the_real_message)) as out_file:
+            out_file.name = "json.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                reply_to=reply_to_id,
+            )
+            await event.delete()
+    else:
+        await eor(event, "`{}`".format(the_real_message))
+
+
 CmdHelp("get_data").add_command(
-  "yaml", None, "Gives out Data of replied msg."
+  "yaml", <reply>, "Gives out Data of replied msg."
+).add_command(
+  "json", "<reply>", "Gets the json data of the replied msg/media from a user/bot/channel"
 ).add_info(
-  "Data Snatch in YAML"
+  "Data Snatch in YAML & JSON"
 ).add_warning(
   "✅ Harmless Module."
 ).add()
