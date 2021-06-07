6b840129e41f3f7a03c023d18c0dbf15248e344d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-07 14:17:46 +0530

    Create memify.py

---
+++ hellbot/plugins/memify.py
@@ -0,0 +1,78 @@
+import cv2
+import os
+import io
+import random
+import shutil
+import re
+import textwrap
+import lottie
+
+from PIL import Image, ImageDraw, ImageEnhance, ImageFont, ImageOps
+
+from . import *
+
+
+path = "./hellmify/"
+if not os.path.isdir(path):
+    os.makedirs(path)
+
+
+@bot.on(hell_cmd(pattern="mmf ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="mmf ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await eod(event, "You need to reply to an image with .mmf` 'text on top' ; 'text on bottom'")
+        return
+    await eor(event, "🤪 **Memifying...**")
+    reply = await event.get_reply_message()
+    imgs = await bot.download_media(reply.media, path)
+    img = cv2.VideoCapture(imgs) 
+    tal, semx = img.read()
+    cv2.imwrite("kraken.webp", semx)
+    text = event.pattern_match.group(1)
+    webp_file = await draw_meme_text("kraken.webp", text)
+    await event.client.send_file(
+        event.chat_id, webp_file, reply_to=event.reply_to_msg_id
+    )
+    await event.delete()
+    shutil.rmtree(path)
+    os.remove("kraken.webp")
+    os.remove(webp_file)
+
+
+@bot.on(hell_cmd(pattern="mms ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="mms ?(.*)", allow_sudo=True))
+async def sed(hellboy):
+    if hellboy.fwd_from:
+        return
+    if not hellboy.reply_to_msg_id:
+        await eod(hellboy, "You need to reply to an image with .mms` 'text on top' ; 'text on bottom'")
+        return
+    await eor(hellboy, "🤪 **Memifying...**")
+    reply = await hellboy.get_reply_message()
+    imgs = await bot.download_media(reply.media, path)
+    img = cv2.VideoCapture(imgs) 
+    tal, semx = img.read()
+    cv2.imwrite("kraken.webp", semx)
+    text = hellboy.pattern_match.group(1)
+    photo = await draw_meme("kraken.webp", text)
+    await hellboy.client.send_file(
+        hellboy.chat_id, photo, reply_to=hellboy.reply_to_msg_id
+    )
+    await hellboy.delete()
+    shutil.rmtree(path)
+    os.remove("kraken.webp")
+    os.remove(photo)
+    
+    
+CmdHelp("memify").add_command(
+  "mmf", "<reply to a img/stcr/gif> <upper text> ; <lower text>", "Memifies the replied image/gif/sticker with your text and sends output in sticker format.", "mmf <reply to a img/stcr/gif> hii ; hello"
+).add_command(
+  "mms", "<reply to a img/stcr/gif> <upper text> ; <lower text>", "Memifies the replied image/gif/sticker with your text and sends output in image format.", "mms <reply to a img/stcr/gif> hii ; hello"
+).add_info(
+  "Make Memes on telegram 😉"
+).add_warning(
+  " Harmless Module."
+).add()
