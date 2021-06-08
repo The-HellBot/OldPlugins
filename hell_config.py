1a5233987046a83c848cc69bb5f8a6d5eede2154
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-08 06:35:11 +0530

    Update hell_config.py

--- hell_config.py
+++ hell_config.py
@@ -67,6 +67,8 @@ class Config(object):
     MAX_MESSAGE_SIZE_LIMIT = 4095
     MAX_SPAM = int(os.environ.get("MAX_SPAM", 3))
     MONGO_URI = os.environ.get("MONGO_URI", None)
+    MY_CHANNEL = os.environ.get("YOUR_CHANNEL", "Its_HellBot")
+    MY_GROUP = os.environ.get("YOUR_GROUP", "HellBot_Chat")
     OCR_API = os.environ.get("OCR_API", None)
     PLUGIN_CHANNEL = os.environ.get("PLUGIN_CHANNEL", None)
     if PLUGIN_CHANNEL:
