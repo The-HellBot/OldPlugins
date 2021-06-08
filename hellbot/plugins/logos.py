ba56e5ada2a15f4f376dd69fa997cb93c0e21d3c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-08 15:32:06 +0530

    Create logos.py

---
+++ hellbot/plugins/logos.py
@@ -0,0 +1,90 @@
+import glob
+import os
+import random
+import time
+
+from PIL import Image, ImageDraw, ImageFont
+from telethon.tl.types import InputMessagesFilterPhotos
+
+from . import *
+
+# Soon : Swith to import fonts from channel....
+FONT_STR = [
+	"./hellbot/resources/fonts/Streamster.ttf",
+	"./hellbot/resources/fonts/Ace Records.ttf",
+	"./hellbot/resources/fonts/Homina.ttf",
+	"./hellbot/resources/fonts/Intro_Inline.otf",
+	"./hellbot/resources/fonts/MagmaWave Caps.otf",
+	"./hellbot/resources/fonts/LEVIBRUSH.TTF",
+	"./hellbot/resources/fonts/Megadeth.ttf",
+	"./hellbot/resources/fonts/savatage.ttf",
+	"./hellbot/resources/fonts/hotpizza.ttf",
+	"./hellbot/resources/fonts/Sheeping Dogs.ttf",
+]
+
+PICS_STR = []
+
+
+@bot.on(hell_cmd(pattern=r"logo ?(.*)"))
+@bot.on(sudo_cmd(pattern=r"logo ?(.*)", allow_sudo=True))
+async def lg1(hellevent):
+    event = await eor(hellevent, "`Processing.....`")
+    fnt = random.choice(FONT_STR)
+    if hellevent.reply_to_msg_id:
+        rply = await hellevent.get_reply_message()
+        logo_ = await rply.download_media()
+    else:
+        async for i in bot.iter_messages("@HellBot_Logos", filter=InputMessagesFilterPhotos):
+    	    PICS_STR.append(i)
+        pic = random.choice(PICS_STR)
+        logo_ = await pic.download_media()
+    text = hellevent.pattern_match.group(1)
+    if len(text) <= 8:
+        font_size_ = 150
+        strik = 10
+    elif len(text) >= 9:
+        font_size_ = 50
+        strik = 5
+    else:
+        font_size_ = 130
+        strik = 20
+    if not text:
+        await eod(event, "**Give some text to make a logo !!**")
+        return
+    img = Image.open(logo_)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(fnt, font_size_)
+    image_widthz, image_heightz = img.size
+    w, h = draw.textsize(text, font=font)
+    h += int(h * 0.21)
+    image_width, image_height = img.size
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        text,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    w_ = (image_width - w) / 2
+    h_ = (image_height - h) / 2
+    draw.text(
+        (w_, h_), text, font=font, fill="white", stroke_width=strik, stroke_fill="black"
+    )
+    file_name = "HellBot.png"
+    img.save(file_name, "png")
+    await bot.send_file(
+        hellevent.chat_id,
+        file_name,
+        caption=f"**Made By :** {hell_mention}",
+    )
+    await event.delete()
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+CmdHelp("logos").add_command(
+  "logo", "<reply to pic + text> ot <text>", "Makes a logo with the given text. If replied to a picture makes logo on that else gets random BG."
+).add_info(
+  "Logo Maker."
+).add_warning(
+  "✅ Harmless Module."
+).add()
