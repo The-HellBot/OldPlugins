c04f0ca3892af513d919fbf0ad6f00c9443584db
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-11 19:50:14 +0530

    Update spam.py

--- hellbot/plugins/spam.py
+++ hellbot/plugins/spam.py
@@ -59,8 +59,6 @@ async def spammer(e):
 async def tiny_pic_spam(e):
     sender = await e.get_sender()
     me = await e.client.get_me()
-    if not sender.id == me.id and not SUDO_WALA:
-        return await e.reply("`Sorry sudo users cant access this command..`")
     try:
         await e.delete()
     except:
