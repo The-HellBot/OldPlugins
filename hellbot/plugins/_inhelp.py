3b10e5dedda613f5d931194cc32bc18f279a2a38
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-18 09:24:13 +0530

    Update _inhelp.py

--- hellbot/plugins/_inhelp.py
+++ hellbot/plugins/_inhelp.py
@@ -69,7 +69,7 @@ def button(page, modules):
     for pairs in pairs[page]:
         buttons.append(
             [
-                custom.Button.inline(f"{hell_emoji} " + pair + f" {hell_emoji}, data=f"Information[{page}]({pair})")
+                custom.Button.inline(f"{hell_emoji} " + pair + f" {hell_emoji}", data=f"Information[{page}]({pair})")
                 for pair in pairs
             ]
         )
