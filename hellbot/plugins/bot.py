727b674a3c7edbbf2892ac898dbea49013a8c16f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-22 13:58:15 +0530

    Update bot.py

--- hellbot/plugins/bot.py
+++ hellbot/plugins/bot.py
@@ -72,11 +72,11 @@ async def _(event):
     c = d.split(" ")
     try:
         chat_id = await get_user_id(c[0])
-    except Exception as ex:
-        return await eod(event, "`" + str(ex) = "`")
+    except Exception as e:
+        return await eod(event, f"`{e}`")
     msg = ""
     hunter = await event.get_reply_message()
-    if e.reply_to_msg_id:
+    if event.reply_to_msg_id:
         await bot.send_message(chat_id, hunter)
         await eod(event, "**[Done]**")
     for i in c[1:]:
