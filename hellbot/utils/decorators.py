9af3a070cd4a200147bc96b2eb6fb769e2ef5526
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-23 14:09:07 +0530

    Later bitches

--- hellbot/utils/decorators.py
+++ hellbot/utils/decorators.py
@@ -64,7 +64,7 @@ def hell_cmd(pattern=None, command=None, **args):
     # decides that other users can use it or not
     # hellbot outgoing
     if allow_sudo:
-        args["from_users"] = list(s_ql.all_sudo())
+        args["from_users"] = list(Config.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
         del args["allow_sudo"]
@@ -131,7 +131,7 @@ def sudo_cmd(pattern=None, command=None, **args):
     # outgoing check
     # hellbot
     if allow_sudo:
-        args["from_users"] = list(s_ql.all_sudo())
+        args["from_users"] = list(Config.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
         del args["allow_sudo"]
@@ -202,7 +202,7 @@ def register(**args):
             pass
 
     if allow_sudo:
-        args["from_users"] = list(s_ql.all_sudo())
+        args["from_users"] = list(Config.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
         del args["allow_sudo"]
@@ -268,7 +268,7 @@ def command(**args):
         except BaseException:
             pass
     if allow_sudo:
-        args["from_users"] = list(s_ql.all_sudo())
+        args["from_users"] = list(Config.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
     del allow_sudo
