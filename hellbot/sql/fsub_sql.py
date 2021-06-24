3e7621f20cd356417a78a898b3b1564688750561
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-24 23:55:36 +0530

    Create fsub_sql.py

---
+++ hellbot/sql/fsub_sql.py
@@ -0,0 +1,41 @@
+from . import SESSION, BASE
+from sqlalchemy import Column, String
+
+
+class Fsub(BASE):
+    __tablename__ = "fsub"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+
+Fsub.__table__.create(checkfirst=True)
+
+
+def is_fsub(chat_id):
+    try:
+        return SESSION.query(Fsub).filter(Fsub.chat_id == str(chat_id)).one()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def add_fsub(chat_id):
+    adder = Fsub(str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def rem_fsub(chat_id):
+    rem = SESSION.query(Fsub).get(str(chat_id))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
+
+
+def all_fsub():
+    rem = SESSION.query(Fsub).all()
+    SESSION.close()
+    return rem
