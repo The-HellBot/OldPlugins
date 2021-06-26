739c4fff07376eff01fb10896a0233ea5283e883
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-26 21:42:03 +0530

    Update _helper.py

--- hellbot/plugins/_helper.py
+++ hellbot/plugins/_helper.py
@@ -10,6 +10,7 @@ msg = f"""
 **⚡ ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt ⚡**
 
   •        [📑 Repo 📑](https://github.com/The-HellBot/HellBot)
+  •        [🚀 Deploy 🚀](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FThe-HellBot%2FHellBot&template=https%3A%2F%2Fgithub.com%2Fthe-hellbot%2Fhellbot)
 
   •  ©️ {hell_channel} ™
 """
