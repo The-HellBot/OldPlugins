95ecb21e1da2d1117acfe2d8d4bc73255401f4d7
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-07-08 09:36:51 +0530

    Add files via upload

--- hell_config.py
+++ hell_config.py
@@ -29,6 +29,7 @@ class Config(object):
         t_file.close()
     BIO_MSG = os.environ.get("BIO_MSG", "ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt")
     BL_CHAT = set(int(x) for x in os.environ.get("BL_CHAT", "").split())
+    BOT_HANDLER = os.environ.get("BOT_HANDLER", "\/")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
     BUTTONS_IN_HELP = int(os.environ.get("BUTTONS_IN_HELP", 7))
@@ -39,6 +40,8 @@ class Config(object):
     DB_URI = os.environ.get("DATABASE_URL", None)
     DUAL_LOG = os.environ.get("DUAL_LOG", None)
     EMOJI_IN_HELP = os.environ.get("EMOJI_IN_HELP", "🔸")
+    EXTRA = os.environ.get("EXTRA", None)
+    EXTRA_REPO = os.environ.get("EXTRA_REPO", None)
     FBAN_LOG_GROUP = os.environ.get("FBAN_LOG_GROUP", None)
     if FBAN_LOG_GROUP:
         FBAN_LOG_GROUP = int(FBAN_LOG_GROUP)
