72d0db2cf275f92d736309cc3f48f7d45a56154d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-07-15 14:34:16 +0530

    Create pasters.py

---
+++ hellbot/helpers/pasters.py
@@ -0,0 +1,39 @@
+import json
+
+import requests
+
+from hellbot import *
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+    "content-type": "application/json",
+}
+
+async def pasty(message, extension=None):
+    siteurl = "https://pasty.lus.pm/api/v1/pastes"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        purl = (
+            f"https://pasty.lus.pm/{response['id']}.{extension}"
+            if extension
+            else f"https://pasty.lus.pm/{response['id']}.txt"
+        )
+        try:
+            await bot.send_message(
+                Config.LOGGER_ID,
+                f"#PASTE \n\n**Open Paste From** [here]({purl}). \n**Delete that paste by using this token** `{response['deletionToken']}`",
+            )
+        except Exception as e:
+            LOGS.info(str(e))
+        return {
+            "url": purl,
+            "raw": f"https://pasty.lus.pm/{response['id']}/raw",
+            "bin": "Pasty",
+        }
+    return {"error": "Unable to reach pasty.lus.pm"}
+
