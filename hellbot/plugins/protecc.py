9953f0e882fb650e452c0edc3b2d785c016db480
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-07-26 16:36:48 +0530

    Add files via upload

---
+++ hellbot/plugins/protecc.py
@@ -0,0 +1,80 @@
+import asyncio
+import os
+import datetime
+import lottie
+import urllib
+import requests
+from bs4 import BeautifulSoup
+
+from . import *
+
+DELETE_TIMEOUT = 0
+
+def progress(current, total):
+    logger.info(
+        "Downloaded {} of {}\nCompleted {}".format(
+            current, total, (current / total) * 100
+        )
+    )
+
+@bot.on(hell_cmd(pattern=r"pt"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.datetime.now()
+    BASE_URL = "http://images.google.com"
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        previous_message_text = previous_message.message
+        if previous_message.media:
+            downloaded_file_name = await bot.download_media(
+                previous_message, Config.TMP_DOWNLOAD_DIRECTORY
+            )
+            SEARCH_URL = "{}/searchbyimage/upload".format(BASE_URL)
+            multipart = {
+                "encoded_image": (
+                    downloaded_file_name,
+                    open(downloaded_file_name, "rb"),
+                ),
+                "image_content": "",
+            }
+
+            google_rs_response = requests.post(
+                SEARCH_URL, files=multipart, allow_redirects=False
+            )
+            the_location = google_rs_response.headers.get("Location")
+            os.remove(downloaded_file_name)
+        else:
+            previous_message_text = previous_message.message
+            SEARCH_URL = "{}/searchbyimage?image_url={}"
+            request_url = SEARCH_URL.format(BASE_URL, previous_message_text)
+            google_rs_response = requests.get(request_url, allow_redirects=False)
+            the_location = google_rs_response.headers.get("Location")
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
+        }
+        response = requests.get(the_location, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        prs_div = soup.find_all("div", {"class": "r5a77d"})[0]
+        prs_anchor_element = prs_div.find("a")
+        prs_url = BASE_URL + prs_anchor_element.get("href")
+        prs_text = prs_anchor_element.text
+
+        img_size_div = soup.find(id="jHnbRc")
+        img_size = img_size_div.find_all("div")
+        end = datetime.datetime.now()
+        ms = (end - start).seconds
+        OUTPUT_STR = """/protecc {prs_text}""".format(
+            **locals())
+    await event.delete()
+    await event.reply(OUTPUT_STR, parse_mode="HTML", link_preview=False)
+
+CmdHelp("protecc").add_command(
+  "pt", "<reply>", "Auto Protecc the waifu."
+).add_info(
+  "Waifu Protecc."
+).add_warning(
+  "✅ Harmless Module."
+).add()
