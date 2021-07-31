089d52bd8e37f67676ddff034fa26250de4a5934
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-01 00:03:12 +0530

    Update protecc.py

--- hellbot/plugins/protecc.py
+++ hellbot/plugins/protecc.py
@@ -4,9 +4,12 @@ import datetime
 import lottie
 import urllib
 import requests
+from asyncio import sleep
 from bs4 import BeautifulSoup
 
 from . import *
+
+qt = "A qt waifu appeared!"
 
 def progress(current, total):
     logger.info(
@@ -66,6 +69,45 @@ async def _(event):
         OUTPUT_STR = """/protecc {prs_text}""".format(
             **locals())
     await hell.edit(OUTPUT_STR, parse_mode="HTML", link_preview=False)
+
+
+@bot.on(events.NewMessage(incoming=True))
+async def reverse(event):
+    if not event.media:
+        return
+    if not qt in event.text:
+        return
+    if not event.sender_id == 792028928:
+        return
+    if Config.WAIFU_CATCHER != "TRUE":
+        return
+    dl = await bot.download_media(event.media, "resources/")
+    file = {"encoded_image": (dl, open(dl, "rb"))}
+    grs = requests.post(
+        "https://www.google.com/searchbyimage/upload", files=file, allow_redirects=False
+    )
+    loc = grs.headers.get("Location")
+    response = requests.get(
+        loc,
+        headers={
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
+        },
+    )
+    qtt = BeautifulSoup(response.text, "html.parser")
+    div = qtt.find_all("div", {"class": "r5a77d"})[0]
+    alls = div.find("a")
+    text = alls.text
+    try:
+        if "cg" in text:
+            return
+        if "fictional character" in text:
+            return
+    except:
+        pass
+    await bot.send_message(event.chat_id, f"/protecc@loli_harem_bot {text}")
+    await sleep(2)
+    os.remove(dl)
+
 
 CmdHelp("protecc").add_command(
   "pt", "<reply>", "Auto Protecc the waifu."
