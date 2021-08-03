e75da47b2ee5930f11535934dc9bf131756a276e
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-03 15:28:55 +0530

    Update hell_config.py

--- hell_config.py
+++ hell_config.py
@@ -40,6 +40,7 @@ class Config(object):
     DB_URI = os.environ.get("DATABASE_URL", None)
     DUAL_LOG = os.environ.get("DUAL_LOG", None)
     EMOJI_IN_HELP = os.environ.get("EMOJI_IN_HELP", "🔸")
+    EXCLUDE_WAIFU = os.environ.get("EXCLUDE_WAIFU", None)
     EXTRA = os.environ.get("EXTRA", None)
     EXTRA_REPO = os.environ.get("EXTRA_REPO", None)
     FBAN_LOG_GROUP = os.environ.get("FBAN_LOG_GROUP", None)
