b65855884a2d9b3b165d5386f989a0f2d41b9bdc
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-10 14:16:22 +0530

    Update anime.py

--- hellbot/helpers/anime.py
+++ hellbot/helpers/anime.py
@@ -1,3 +1,5 @@
+import json
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -108,3 +110,66 @@ def parse_filler(filler_id):
             "ac_ep": ", ".join(ac_ep),
         }
         return dict_
+
+async def callAPI(search_str):
+    query = """
+    query ($id: Int,$search: String) { 
+      Media (id: $id, type: ANIME,search: $search) { 
+        id
+        title {
+          romaji
+          english
+        }
+        description (asHtml: false)
+        startDate{
+            year
+          }
+          episodes
+          chapters
+          volumes
+          season
+          type
+          format
+          status
+          duration
+          averageScore
+          genres
+          bannerImage
+      }
+    }
+    """
+    variables = {"search": search_str}
+    url = "https://graphql.anilist.co"
+    response = requests.post(url, json={"query": query, "variables": variables})
+    return response.text
+
+
+async def formatJSON(outData):
+    msg = ""
+    jsonData = json.loads(outData)
+    res = list(jsonData.keys())
+    if "errors" in res:
+        msg += f"**Error** : `{jsonData['errors'][0]['message']}`"
+        return msg
+    else:
+        jsonData = jsonData["data"]["Media"]
+        if "bannerImage" in jsonData.keys():
+            msg += f"[〽️]({jsonData['bannerImage']})"
+        else:
+            msg += "〽️"
+        title = jsonData["title"]["romaji"]
+        link = f"https://anilist.co/anime/{jsonData['id']}"
+        msg += f"[{title}]({link})"
+        msg += f"\n\n**Type** : {jsonData['format']}"
+        msg += f"\n**Genres** : "
+        for g in jsonData["genres"]:
+            msg += g + " "
+        msg += f"\n**Status** : {jsonData['status']}"
+        msg += f"\n**Episode** : {jsonData['episodes']}"
+        msg += f"\n**Year** : {jsonData['startDate']['year']}"
+        msg += f"\n**Score** : {jsonData['averageScore']}"
+        msg += f"\n**Duration** : {jsonData['duration']} min\n\n"
+        # https://t.me/catuserbot_support/19496
+        descr = f"{jsonData['description']}"
+        msg += " __" + re.sub("<br>", "\n", descr) + "__"
+        return msg
