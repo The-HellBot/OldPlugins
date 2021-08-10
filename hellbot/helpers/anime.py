84f7345ae69736cde263f6ff968bf0a2fccf313d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-10 14:05:46 +0530

    Create anime.py

---
+++ hellbot/helpers/anime.py
@@ -0,0 +1,110 @@
+import requests
+from bs4 import BeautifulSoup
+
+def search_filler(query):
+    html = requests.get("https://www.animefillerlist.com/shows").text
+    soup = BeautifulSoup(html, "html.parser")
+    div = soup.findAll("div", attrs={"class": "Group"})
+    index = {}
+    for i in div:
+        li = i.findAll("li")
+        for jk in li:
+            yum = jk.a["href"].split("/")[-1]
+            cum = jk.text
+            index[cum] = yum
+    ret = {}
+    keys = list(index.keys())
+    for i in range(len(keys)):
+        if query.lower() in keys[i].lower():
+            ret[keys[i]] = index[keys[i]]
+    return ret
+
+
+def parse_filler(filler_id):
+    url = "https://www.animefillerlist.com/shows/" + filler_id
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+    div = soup.find("div", attrs={"id": "Condensed"})
+    all_ep = div.find_all("span", attrs={"class": "Episodes"})
+    if len(all_ep) == 1:
+        ttl_ep = all_ep[0].findAll("a")
+        total_ep = []
+        mix_ep = None
+        filler_ep = None
+        ac_ep = None
+        for tol in ttl_ep:
+            total_ep.append(tol.text)
+        dict_ = {
+            "filler_id": filler_id,
+            "total_ep": ", ".join(total_ep),
+            "mixed_ep": mix_ep,
+            "filler_ep": filler_ep,
+            "ac_ep": ac_ep
+        }
+        return dict_
+    if len(all_ep) == 2:
+        ttl_ep = all_ep[0].findAll("a")
+        fl_ep = all_ep[1].findAll("a")
+        total_ep = []
+        mix_ep = None
+        ac_ep = None
+        filler_ep = []
+        for tol in ttl_ep:
+            total_ep.append(tol.text)
+        for fol in fl_ep:
+            filler_ep.append(fol.text)
+        dict_ = {
+            "filler_id": filler_id,
+            "total_ep": ", ".join(total_ep),
+            "mixed_ep": mix_ep,
+            "filler_ep": ", ".join(filler_ep),
+            "ac_ep": ac_ep
+        }
+        return dict_
+    if len(all_ep) == 3:
+        ttl_ep = all_ep[0].findAll("a")
+        mxl_ep = all_ep[1].findAll("a")
+        fl_ep = all_ep[2].findAll("a")
+        total_ep = []
+        mix_ep = []
+        filler_ep = []
+        ac_ep = None
+        for tol in ttl_ep:
+            total_ep.append(tol.text)
+        for fol in fl_ep:
+            filler_ep.append(fol.text)
+        for mol in mxl_ep:
+            mix_ep.append(mol.text)
+        dict_ = {
+            "filler_id": filler_id,
+            "total_ep": ", ".join(total_ep),
+            "mixed_ep": ", ".join(mix_ep),
+            "filler_ep": ", ".join(filler_ep),
+            "ac_ep": ac_ep
+        }
+        return dict_
+    if len(all_ep) == 4:
+        ttl_ep = all_ep[0].findAll("a")
+        mxl_ep = all_ep[1].findAll("a")
+        fl_ep = all_ep[2].findAll("a")
+        al_ep = all_ep[3].findAll("a")
+        total_ep = []
+        mix_ep = []
+        filler_ep = []
+        ac_ep = []
+        for tol in ttl_ep:
+            total_ep.append(tol.text)
+        for fol in fl_ep:
+            filler_ep.append(fol.text)
+        for mol in mxl_ep:
+            mix_ep.append(mol.text)
+        for aol in al_ep:
+            ac_ep.append(aol.text)
+        dict_ = {
+            "filler_id": filler_id,
+            "total_ep": ", ".join(total_ep),
+            "mixed_ep": ", ".join(mix_ep),
+            "filler_ep": ", ".join(filler_ep),
+            "ac_ep": ", ".join(ac_ep),
+        }
+        return dict_
