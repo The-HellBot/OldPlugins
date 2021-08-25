7d25cc885e08ef8fedd49485f48feaf035ad6c2a
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-25 23:07:03 +0530

    Update yt_helper.py

--- hellbot/helpers/yt_helper.py
+++ hellbot/helpers/yt_helper.py
@@ -13,9 +13,9 @@ async def song_search(event, query, max_results, details=False):
     for i in results["videos"]:
         x += f"https://www.youtube.com{i['url_suffix']}\n"
     if details:
-        title = results["title"]
-        views = results["views"]
-        duration = results["duration"]
-        thumb = results["thumbnail"][0]
+        title = results[0]["title"]
+        views = results[0]["views"]
+        duration = results[0]["duration"]
+        thumb = results[0]["thumbnail"][0]
         return x, title, views, duration, thumb
     return x
