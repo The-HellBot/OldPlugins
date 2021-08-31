d9617d072a3d7e6a4d30767251d8550abc985dd0
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-08-31 13:33:22 +0530

    Update vars.py

--- hellbot/config/vars.py
+++ hellbot/config/vars.py
@@ -6,7 +6,7 @@ import os
 if os.path.exists("config.py"):
     from config import Development as Config
 else:
-    from hell_config import Config
+    from .hell_config import Config
 
 
 # hellbot
