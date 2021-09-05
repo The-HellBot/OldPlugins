c5dd8016abcd9e010b144699c5cbc4fd314684ae
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-05 10:42:35 +0530

    Update maths.py

--- hellbot/plugins/maths.py
+++ hellbot/plugins/maths.py
@@ -5,7 +5,7 @@ from . import *
 @bot.on(hell_cmd(pattern="sin ?(.*)"))
 @bot.on(sudo_cmd(pattern="sin ?(.*)", allow_sudo=True))
 async def findsin(event):
-    input_str = int(event.pattern_match.group(1))
+    input_str = float(event.text[5:])
     output = math.sin(input_str)
     await event.edit(f"**Value of Sin** `{input_str}`==\n`{output}`")
 
@@ -13,7 +13,7 @@ async def findsin(event):
 @bot.on(hell_cmd(pattern="cos ?(.*)"))
 @bot.on(sudo_cmd(pattern="cos ?(.*)", allow_sudo=True))
 async def find_cos(event):
-    input_str = int(event.pattern_match.group(1))
+    input_str = float(event.text[5:])
     output = math.cos(input_str)
     await event.edit(f"**Value of Cos** `{input_str}`==\n`{output}`")
 
@@ -21,7 +21,7 @@ async def find_cos(event):
 @bot.on(hell_cmd(pattern="tan ?(.*)"))
 @bot.on(sudo_cmd(pattern="tan ?(.*)", allow_sudo=True))
 async def find_tan(event):
-    input_str = int(event.pattern_match.group(1))
+    input_str = float(event.text[5:])
     output = math.tan(input_str)
     await event.edit(f"**Value of Tan** `{input_str}`==\n`{output}`")
 
@@ -29,7 +29,7 @@ async def find_tan(event):
 @bot.on(hell_cmd(pattern="cosec ?(.*)"))
 @bot.on(sudo_cmd(pattern="cosec ?(.*)", allow_sudo=True))
 async def find_csc(event):
-    input_str = float(event.pattern_match.group(1))
+    input_str = float(event.text[7:])
     output = mpmath.csc(input_str)
     await event.edit(f"**Value of Cosec** `{input_str}`==\n`{output}`")
 
@@ -37,7 +37,7 @@ async def find_csc(event):
 @bot.on(hell_cmd(pattern="sec ?(.*)"))
 @bot.on(sudo_cmd(pattern="sec ?(.*)", allow_sudo=True))
 async def find_sec(event):
-    input_str = float(event.pattern_match.group(1))
+    input_str = float(event.text[5:])
     output = mpmath.sec(input_str)
     await event.edit(f"**Value of Sec** `{input_str}`==\n`{output}`")
 
@@ -45,7 +45,7 @@ async def find_sec(event):
 @bot.on(hell_cmd(pattern="cot ?(.*)"))
 @bot.on(sudo_cmd(pattern="cot ?(.*)", allow_sudo=True))
 async def find_cot(event):
-    input_str = float(event.pattern_match.group(1))
+    input_str = float(event.text[5:])
     output = mpmath.cot(input_str)
     await event.edit(f"**Value of Cot** `{input_str}`==\n`{output}`")
 
@@ -53,7 +53,7 @@ async def find_cot(event):
 @bot.on(hell_cmd(pattern="square ?(.*)"))
 @bot.on(sudo_cmd(pattern="square ?(.*)", allow_sudo=True))
 async def square(event):
-    input_str = float(event.pattern_match.group(1))
+    input_str = float(event.text[8:])
     output = input_str * input_str
     await event.edit(f"**Square of** `{input_str}`==\n`{output}`")
 
@@ -61,7 +61,7 @@ async def square(event):
 @bot.on(hell_cmd(pattern="cube ?(.*)"))
 @bot.on(sudo_cmd(pattern="cube ?(.*)", allow_sudo=True))
 async def cube(event):
-    input_str = float(event.pattern_match.group(1))  
+    input_str = float(event.text[6:])  
     output = input_str * input_str * input_str
     await event.edit(f"**Cube of** `{input_str}`==\n`{output}`")
 
