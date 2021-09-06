c9b27f8e9bc45cd9df36bd85d532b133fce9f3d7
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-06 16:36:52 +0530

    Update __init__.py

--- hellbot/__init__.py
+++ hellbot/__init__.py
@@ -6,10 +6,7 @@ import time
 from distutils.util import strtobool as sb
 from logging import DEBUG, INFO, basicConfig, getLogger
 
-from telethon import TelegramClient
-from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
-from telethon.sessions import StringSession
-
+from hellbot.clients import *
 from hellbot.config import Config
 
 
@@ -28,37 +25,6 @@ else:
 
 
 LOGS = getLogger(__name__)
-
-
-if Config.HELLBOT_SESSION:
-    session = StringSession(str(Config.HELLBOT_SESSION))
-else:
-    session = "hellbot"
-
-
-try:
-    Hell = TelegramClient(
-        session=session,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-except Exception as e:
-    print(f"HELLBOT_SESSION - {e}")
-    sys.exit()
-
-
-HellBot = TelegramClient(
-    session="Hell-TBot",
-    api_id=Config.APP_ID,
-    api_hash=Config.API_HASH,
-    connection=ConnectionTcpAbridged,
-    auto_reconnect=True,
-    connection_retries=None,
-).start(bot_token=Config.BOT_TOKEN)
-
 
 bot = Hell
 tbot = HellBot
