978fce0070697dc5f133434fd7b42f96c874adcd
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-06 20:25:03 +0530

    Update __init__.py

--- hellbot/__init__.py
+++ hellbot/__init__.py
@@ -29,6 +29,42 @@ LOGS = getLogger(__name__)
 bot = Hell
 tbot = HellBot
 
+
+if not Config.API_HASH:
+    LOGS.warning("Please fill var API HASH to continue.")
+    quit(1)
+
+
+if not Config.APP_ID:
+    LOGS.warning("Please fill var APP ID to continue.")
+    quit(1)
+
+
+if not Config.BOT_TOKEN:
+    LOGS.warning("Please fill var BOT TOKEN to continue.")
+    quit(1)
+
+
+if not Config.BOT_USERNAME:
+    LOGS.warning("Please fill var BOT USERNAME to continue.")
+    quit(1)
+
+
+if not Config.DB_URI:    
+    LOGS.warning("Please fill var DATABASE URL to continue.")
+    quit(1)
+
+
+if not Config.HELLBOT_SESSION:
+    LOGS.warning("Please fill var HELLBOT SESSION to continue.")
+    quit(1)
+
+
+if not Config.LOGGER_ID:
+    LOGS.warning("Please fill var LOGGER ID to continue.")
+    quit(1)
+
+
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
         HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
