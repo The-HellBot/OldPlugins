144ee8dc3969c0c172deba0ccb8840652009cab4
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-07 15:00:22 +0530

    Update client_list.py

--- hellbot/clients/client_list.py
+++ hellbot/clients/client_list.py
@@ -1,4 +1,3 @@
-from hellbot import bot
 from hellbot.clients import Hell, H2, H3, H4, H5
 from hellbot.config import Config
 
@@ -7,7 +6,7 @@ async def clients_list(Config, Hell, H2, H3, H4, H5):
     user_ids = []
     if Config.SUDO_USERS:
         user_ids.append(list(Config.SUDO_USERS))
-    main_id = await bot.get_me()
+    main_id = await Hell.get_me()
     user_ids.append(main_id.id)
     if H2:
         id2 = await H2.get_me()
