0c7dc6fe9b3b3a6b47f69c5673689c198780658d
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-07 14:08:19 +0530

    Update alive.py

--- hellbot/plugins/alive.py
+++ hellbot/plugins/alive.py
@@ -25,11 +25,12 @@ ALIVE_TEMP = """
 """
 #-------------------------------------------------------------------------------
 
-@bot.on(hell_cmd(pattern="alive$", allow_sudo=True))
+@H1.on(hell_cmd(pattern="alive$"))
 @H2.on(hell_cmd(pattern="alive$"))
 @H3.on(hell_cmd(pattern="alive$"))
 @H4.on(hell_cmd(pattern="alive$"))
 @H5.on(hell_cmd(pattern="alive$"))
+@bot.on(sudo_cmd(pattern="alive$"))
 async def up(event):
     start = datetime.datetime.now()
     hell = await eor(event, "`Building Alive....`")
