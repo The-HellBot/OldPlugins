ba79c8051305482afb24725951afba0a5c0d2006
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-11 21:56:06 +0530

    Update decorators.py

--- hellbot/utils/decorators.py
+++ hellbot/utils/decorators.py
@@ -72,18 +72,18 @@ def hell_cmd(
 
     def decorator(func):
         if not disable_edited:
-            bot.add_event_handler(func, events.MessageEdited(pattern=hell_reg, **args))
-        bot.add_event_handler(func, events.NewMessage(pattern=hell_reg, **args))
+            bot.add_event_handler(func, events.MessageEdited(**args))
+        bot.add_event_handler(func, events.NewMessage(**args))
         if allow_sudo:
-            bot.add_event_handler(func, events.NewMessage(pattern=sudo_reg, from_users=list(Config.SUDO_USERS), **args))
+            bot.add_event_handler(func, events.NewMessage(from_users=list(Config.SUDO_USERS), **args))
         if H2:
-            H2.add_event_handler(func, events.NewMessage(pattern=hell_reg, **args))
+            H2.add_event_handler(func, events.NewMessage(**args))
         if H3:
-            H3.add_event_handler(func, events.NewMessage(pattern=hell_reg, **args))
+            H3.add_event_handler(func, events.NewMessage(**args))
         if H4:
-            H4.add_event_handler(func, events.NewMessage(pattern=hell_reg, **args))
+            H4.add_event_handler(func, events.NewMessage(**args))
         if H5:
-            H5.add_event_handler(func, events.NewMessage(pattern=hell_reg, **args))
+            H5.add_event_handler(func, events.NewMessage(**args))
         try:
             LOAD_PLUG[file_test].append(func)
         except Exception:
