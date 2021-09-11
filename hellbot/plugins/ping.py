a516ca2aa8939974859b0b3148e640b873a8ae3d
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-12 01:10:43 +0530

    Update ping.py

--- hellbot/plugins/ping.py
+++ hellbot/plugins/ping.py
@@ -1,5 +1,5 @@
-
+from . import ForGo10God, HELL_USER
 
 @hell_cmd(pattern="ping$")
 async def _(event):
-    await eor(event, f"Pong \n\n{ForGo10God}\n\n{HELL_NAME}")
+    await eor(event, f"Pong \n\n{ForGo10God}\n\n{HELL_USER}")
