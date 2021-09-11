9beaa58cdd3064ad45038ffa64a256c40e835e82
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-12 01:00:27 +0530

    Update session.py

--- hellbot/clients/session.py
+++ hellbot/clients/session.py
@@ -26,10 +26,6 @@ except Exception as e:
 
 if Config.SESSION_2:
     session2 = StringSession(str(Config.SESSION_2))
-else:
-    session2 = "hellbot2"
-
-try:
     H2 = TelegramClient(
         session=session2,
         api_id=Config.APP_ID,
@@ -38,16 +34,12 @@ try:
         auto_reconnect=True,
         connection_retries=None,
     )
-except:
+else:
     H2 = None
 
 
 if Config.SESSION_3:
     session3 = StringSession(str(Config.SESSION_3))
-else:
-    session3 = "hellbot3"
-
-try:
     H3 = TelegramClient(
         session=session3,
         api_id=Config.APP_ID,
@@ -56,16 +48,12 @@ try:
         auto_reconnect=True,
         connection_retries=None,
     )
-except:
+else:
     H3 = None
 
 
 if Config.SESSION_4:
     session4 = StringSession(str(Config.SESSION_4))
-else:
-    session4 = "hellbot4"
-
-try:
     H4 = TelegramClient(
         session=session4,
         api_id=Config.APP_ID,
@@ -74,16 +62,12 @@ try:
         auto_reconnect=True,
         connection_retries=None,
     )
-except:
+else:
     H4 = None
 
 
 if Config.SESSION_5:
     session5 = StringSession(str(Config.SESSION_5))
-else:
-    session5 = "hellbot5"
-
-try:
     H5 = TelegramClient(
         session=session5,
         api_id=Config.APP_ID,
@@ -92,7 +76,7 @@ try:
         auto_reconnect=True,
         connection_retries=None,
     )
-except:
+else:
     H5 = None
 
 
