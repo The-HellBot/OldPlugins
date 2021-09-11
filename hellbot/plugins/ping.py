27f568eecee1582593ed4f57847441ee2b31e96f
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-12 01:28:23 +0530

    Update ping.py

--- hellbot/plugins/ping.py
+++ hellbot/plugins/ping.py
@@ -3,8 +3,8 @@ import telethon.utils
 
 @hell_cmd(pattern="ping$")
 async def _(event):
-    client.me = await event.client.get_me()
-    client.uid = telethon.utils.get_peer_id(client.me)
-    ForGo10God = client.uid
-    HELL_USER = client.me.first_name
+    client = await bot.get_me()
+    uid = telethon.utils.get_peer_id(client)
+    ForGo10God = uid
+    HELL_USER = client.first_name
     await eor(event, f"Pong \n\n{ForGo10God}\n\n{HELL_USER}")
