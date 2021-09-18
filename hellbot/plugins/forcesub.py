72cab13baf520f76f2b47171cec8ef860a0c3dfe
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-18 20:16:59 +0530

    Update forcesub.py

--- hellbot/plugins/forcesub.py
+++ hellbot/plugins/forcesub.py
@@ -3,6 +3,7 @@ from telethon import Button
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.functions.messages import ExportChatInviteRequest
+from telethon.tl.functions.users import GetFullUserRequest
 
 from hellbot.sql.fsub_sql import *
 from . import *
