06861395c7cf1663b89aec20e7e7cfeb32a56883
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-18 20:30:39 +0530

    Update decs.py

--- hellbot/clients/decs.py
+++ hellbot/clients/decs.py
@@ -82,3 +82,21 @@ def hell_cmd(
         return func
 
     return decorator
+
+
+def hell_handler(**args):
+    def decorator(func):
+        async def wrapper(event):
+            await func(event)
+        bot.add_event_handler(wrapper, events.NewMessage(**args))
+        if H2:
+            H2.add_event_handler(wrapper, events.NewMessage(**args))
+        if H3:
+            H3.add_event_handler(wrapper, events.NewMessage(**args))
+        if H4:
+            H4.add_event_handler(wrapper, events.NewMessage(**args))
+        if H5:
+            H5.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorater
