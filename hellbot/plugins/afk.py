a7a2af67894b4352d38b500e7f2205494223eef1
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-22 11:14:53 +0530

    Update afk.py

--- hellbot/plugins/afk.py
+++ hellbot/plugins/afk.py
@@ -10,22 +10,23 @@ from . import *
 
 global afk_time
 global last_afk_message
-global afk_stat
+global afk_start
 global afk_end
 afk_time = None
 last_afk_message = {}
-afk_stat = None
+afk_start = {}
+
 
 @H1.on(events.NewMessage(outgoing=True))
 async def set_not_afk(event):
     global afk_time
     global last_afk_message
-    global afk_stat
+    global afk_start
     global afk_end
     came_back = datetime.datetime.now()
     afk_end = came_back.replace(microsecond=0)
-    if afk_stat is not None:
-        total_afk_time = str((afk_end - afk_stat))
+    if afk_start != {}:
+        total_afk_time = str((afk_end - afk_start))
     current_message = event.message.message
     if "#" not in current_message and gvarstat("AFK") == "YES":
         hellbot = await event.client.send_message(
@@ -66,12 +67,12 @@ async def set_not_afk(event):
 async def on_afk(event):
     global afk_time
     global last_afk_message
-    global afk_stat
+    global afk_start
     global afk_end
     cum_back = datetime.datetime.now()
     afk_end = cum_back.replace(microsecond=0)
-    if afk_stat is not None:
-        total_afk_time = str((afk_end - afk_stat))
+    if afk_start != {}:
+        total_afk_time = str((afk_end - afk_start))
     current_message_text = event.message.message.lower()
     if "afk" in current_message_text:
         return False
@@ -102,7 +103,7 @@ async def _(event):
     krakenop = await event.get_reply_message()
     global afk_time
     global last_afk_message
-    global afk_stat
+    global afk_start
     global afk_end
     global reason
     global hellpic
@@ -111,8 +112,6 @@ async def _(event):
     afk_end = {}
     start_1 = datetime.datetime.now()
     afk_start = start_1.replace(microsecond=0)
-    addgvar("AFK_START", afk_start)
-    afk_stat = int(gvarstat("AFK_START"))
     owo = event.text[5:]
     reason = owo
     hellpic = await event.client.download_media(krakenop)
