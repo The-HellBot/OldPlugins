0dd2973aa1aaa3c6cf532e0cc68728e08726259f
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-28 12:07:56 +0530

    update ping.py

--- hellbot/plugins/ping.py
+++ hellbot/plugins/ping.py
@@ -1,10 +1,31 @@
-import telethon.utils
+import asyncio
+import datetime
+import time
 
-from . import client_id
+from . import *
 
+ping_txt = """<b><i><u>╰•★★  ℘ơŋɠ ★★•╯</b></i></u>
+    ⚘  <b>ʂ℘ɛɛɖ :</b> <code>{ms}</code>
+    ⚘  <b>ų℘ɬıɱɛ :</b> <code>{uptime}</code>
+    ⚘  <b>ơῳŋɛཞ :</b> {hell_mention}"""
 
 @hell_cmd(pattern="ping$")
-async def p(event):
-    x = await client_id(event)
-    ForGo10God, HELL_USER, hell_mention = x[0], x[1], x[2]
-    await eor(event, f"**Pong !!**\n\n{ForGo10God} \n\n{HELL_USER} \n\n{hell_mention}")
+async def pong(hell):
+    start = datetime.datetime.now()
+    event = await eor(hell, "`·.·★ ℘ıŋɠ ★·.·´")
+    cid = await client_id(event)
+    ForGo10God, HELL_USER = cid[0], cid[1]
+    hell_mention = f"<a href='{ForGo10God}'>{HELL_USER}</a>"
+    uptime = await get_time((time.time() - StartTime))
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(ping_txt.format(ms, uptime, hell_mention), parse_mode="HTML")
+
+
+CmdHelp("ping").add_command(
+  "ping", None, "Checks the ping speed of your Hêllẞø†"
+).add_warning(
+  "✅ Harmless Module"
+).add()
+
+# hellbot
