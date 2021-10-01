2d49bb2ebaa2c2e467f6816616e2a66db2047605
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-10-01 15:17:01 +0530

    update qbot.py

--- hellbot/plugins/qbot.py
+++ hellbot/plugins/qbot.py
@@ -7,14 +7,29 @@ async def _(event):
         await eod(event, "```Reply to any user's message.```")
         return
     reply_message = await event.get_reply_message()
-    colour = event.text[4:] or "#1b1429"
+    clr = event.text[4:]
+    colour = clr.replace("'", "")
+    limit = event.text[-2:]
+    to_quote = []
+    if limit and limit.isnumeric():
+        to_quote.append(reply_message.id)
+        async for to_qt in event.client.iter_messages(
+            event.chat_id,
+            limit=(int(limit) - 1),
+            offset_id=reply_message.id,
+            reverse=True,
+        ):
+            if to_qt.id != event.id:
+                to_quote.append(to_qt.id)
+    else:
+        to_quote.append(reply_message.id)
     chat = "@QuotLyBot"
     hell = await eor(event, "```Making a Quote...```")
     async with event.client.conversation(chat) as conv:
         try:
             first = await conv.send_message(f"/qcolor {colour}")
             second = await conv.get_response()
-            third = await event.client.forward_messages(chat, reply_message)
+            third = await event.client.forward_messages(chat, to_quote, event.chat_id)
             fourth = await conv.get_response()
         except YouBlockedUserError:
             await hell.edit("Please unblock @QuotLyBot and try again!!")
@@ -26,7 +41,7 @@ async def _(event):
     )
 
 CmdHelp("qbot").add_command(
-  "ss", "<reply to msg> <bg colour>", "Makes the sticker of the replied text, sticker, pic.", "ss black <reply to a msg>"
+  "ss", "<reply to msg> '<bg colour>' <number of msgs>", "Makes the sticker of the replied text, sticker, pic till next given count msgs.", "ss 'black' 05 <reply to a msg>"
 ).add_info(
   "Makes Quoted Sticker."
 ).add_warning(
