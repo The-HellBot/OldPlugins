92f7d8f6441710133ec3673bb53eaa20eb46fbef
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-10-02 21:31:49 +0530

    update updater.py

--- hellbot/plugins/updater.py
+++ hellbot/plugins/updater.py
@@ -148,7 +148,7 @@ async def upstream(event):
         _version, _release, _branch, _author, _auturl = await hell_info(hellbot_info)
         output_ = f"**Your Bot Version :** `{hell_ver}` \n**Owner :** {hell_mention} \n\n**Official HellBot Version :** `{_version}` \n**Release Date :** `{_release}` \n**Official Repo Branch :** `{_branch}` \n**Update By :** [{_author}]({_auturl})"
         if str(_version) not in str(hell_ver):
-            output_ += f"\n\n**Do** `{hl}update deploy` **to update your HellBot to latest version.**"
+            output_ += f"\n\n**Do** `{hl}update build` **to update your HellBot to latest version.**"
         await event.edit(output_)
         return repo.__del__()
     if conf == "" and not force_update:
@@ -187,7 +187,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         _version, _release, _branch, _author, _auturl = await hell_info(hellbot_info)
-        await event.edit(f"<b><i>Hêllẞø† Docker Build In Progress !!</b></i> \n\n<b><i><u>Update Information :</b></i></u> \n<b>• Branch :</b> {_branch} \n<b>• Release Date :</b> {_release} \n<b>• Version :</b> {_version} \n<b>• Author :</b> <a href='{_auturl}'>{_author}</a>", link_preview=False, parse_mode="HTML")
+        await event.edit(f"<b><i>Hêllẞø† Docker Build In Progress !!!</b></i> \n\n<b><i><u>Update Information :</b></i></u> \n<b>• Branch :</b> {_branch} \n<b>• Release Date :</b> {_release} \n<b>• Version :</b> {_version} \n<b>• Author :</b> <a href='{_auturl}'>{_author}</a>", link_preview=False, parse_mode="HTML")
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
         heroku_git_url = heroku_app.git_url.replace(
@@ -210,7 +210,6 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             await asyncio.sleep(5)
             return await event.delete()
-        _version, _release, _branch, _author, _auturl = await hell_info(hellbot_info)
         await event.edit(f"**Your Hêllẞø† Is UpToDate**\n\n**Version :**  __{hell_ver}__\n**Oɯɳҽɾ :**  {hell_mention}")
     else:
         await event.edit("**Please set up**  `HEROKU_API_KEY`  **from heroku to update!**")
