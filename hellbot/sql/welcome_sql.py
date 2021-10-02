20b1e54eb953590867a0100cc54ce8f9751cb3ec
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-10-02 20:01:51 +0530

    Update welcome_sql.py

--- hellbot/sql/welcome_sql.py
+++ hellbot/sql/welcome_sql.py
@@ -6,7 +6,6 @@ from sqlalchemy import BigInteger, Column, Numeric, String, UnicodeText
 class Welcome(BASE):
     __tablename__ = "welcome"
     chat_id = Column(String(14), primary_key=True)
-    client = Column(String(14), primary_key=True)
     previous_welcome = Column(BigInteger)
     reply = Column(UnicodeText)
     f_mesg_id = Column(Numeric)
@@ -16,46 +15,45 @@ class Welcome(BASE):
         self.previous_welcome = previous_welcome
         self.reply = reply
         self.f_mesg_id = f_mesg_id
-        self.client = str(client)
 
 
 Welcome.__table__.create(checkfirst=True)
 
 
-def get_welcome(chat_id, client):
+def get_welcome(chat_id):
     try:
-        return SESSION.query(Welcome).get(str(chat_id), str(client))
+        return SESSION.query(Welcome).get(str(chat_id))
     finally:
         SESSION.close()
 
 
-def get_current_welcome(chat_id, client):
+def get_current_welcome(chat_id):
     try:
-        return SESSION.query(Welcome).filter(Welcome.chat_id == str(chat_id), str(client)).one()
+        return SESSION.query(Welcome).filter(Welcome.chat_id == str(chat_id)).one()
     except BaseException:
         return None
     finally:
         SESSION.close()
 
 
-def add_welcome(chat_id, previous_welcome, reply, f_mesg_id, client):
+def add_welcome(chat_id, previous_welcome, reply, f_mesg_id):
     to_check = get_welcome(chat_id, client)
     if not to_check:
-        adder = Welcome(chat_id, previous_welcome, reply, f_mesg_id, client)
+        adder = Welcome(chat_id, previous_welcome, reply, f_mesg_id)
         SESSION.add(adder)
         SESSION.commit()
         return True
-    rem = SESSION.query(Welcome).get(str(chat_id), str(client))
+    rem = SESSION.query(Welcome).get(str(chat_id))
     SESSION.delete(rem)
     SESSION.commit()
-    adder = Welcome(chat_id, previous_welcome, reply, f_mesg_id, client)
+    adder = Welcome(chat_id, previous_welcome, reply, f_mesg_id)
     SESSION.commit()
     return False
 
 
-def rm_welcome(chat_id, client):
+def rm_welcome(chat_id):
     try:
-        rem = SESSION.query(Welcome).get(str(chat_id), str(client))
+        rem = SESSION.query(Welcome).get(str(chat_id))
         if rem:
             SESSION.delete(rem)
             SESSION.commit()
@@ -64,7 +62,7 @@ def rm_welcome(chat_id, client):
         return False
 
 
-def update_welcome(chat_id, previous_welcome, client):
-    row = SESSION.query(Welcome).get(str(chat_id), str(client))
+def update_welcome(chat_id, previous_welcome):
+    row = SESSION.query(Welcome).get(str(chat_id))
     row.previous_welcome = previous_welcome
     SESSION.commit()
