2fa4937dc59ee991836d24e1a9b30cc66b68c188
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-19 14:58:18 +0530

    Update cmds.py

--- hellbot/utils/cmds.py
+++ hellbot/utils/cmds.py
@@ -60,17 +60,26 @@ class CmdHelp:
 
     def get_result(self):
         result = f"**📗 File :**  `{self.FILE}`\n"
-        if self.INFO == "":
-            if not self.WARNING == "":
-                result += f"**⚠️ Warning :**  {self.WARNING}\n\n"
-        else:
-            if not self.WARNING == "":
-                result += f"**⚠️ Warning :**  {self.WARNING}\n"
-            result += f"**ℹ️ Info :**  {self.INFO}\n\n"
         if self.EXTRA != "":
+            if self.INFO == "":
+                if not self.WARNING == "":
+                    result += f"**⚠️ Warning :**  {self.WARNING}\n\n"
+            else:
+                if not self.WARNING == "":
+                    result += f"**⚠️ Warning :**  {self.WARNING}\n"
+                result += f"**ℹ️ Info :**  {self.INFO}\n"
             for extra in self.EXTRA:
                 extra = self.EXTRA[extra]
-                result += f"**{extra['extra']} :**  `{extra['content']}`\n"
+                result += f"**{extra['extra']} :**  `{extra['content']}`\n\n"
+        else:
+            if self.INFO == "":
+                if not self.WARNING == "":
+                    result += f"**⚠️ Warning :**  {self.WARNING}\n\n"
+            else:
+                if not self.WARNING == "":
+                    result += f"**⚠️ Warning :**  {self.WARNING}\n"
+                result += f"**ℹ️ Info :**  {self.INFO}\n\n"
+
         for command in self.COMMANDS:
             command = self.COMMANDS[command]
             if command["params"] == None:
