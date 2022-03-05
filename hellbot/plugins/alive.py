86c2e636df5413c5d68b0f04bd72be98291b0c7b
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-05 13:51:07 +0530

    Update alive.py

--- hellbot/plugins/alive.py
+++ hellbot/plugins/alive.py
@@ -22,6 +22,15 @@ ALIVE_TEMP = """
 ╰──────────────
 <b><i>»»» <a href='https://t.me/its_hellbot'>[ †hê Hêllẞø† ]</a> «««</i></b>
 """
+
+msg = """{}\n
+<b><i>🏅 𝙱𝚘𝚝 𝚂𝚝𝚊𝚝𝚞𝚜 🏅</b></i>
+<b>Telethon ≈</b>  <i>{}</i>
+<b>Hêllẞø† ≈</b>  <i>{}</i>
+<b>Uptime ≈</b>  <i>{}</i>
+<b>Abuse ≈</b>  <i>{}</i>
+<b>Sudo ≈</b>  <i>{}</i>
+"""
 #-------------------------------------------------------------------------------
 
 @hell_cmd(pattern="alive$")
@@ -32,32 +41,22 @@ async def up(event):
     hell = await eor(event, "`Building Alive....`")
     uptime = await get_time((time.time() - StartTime))
     a = gvarstat("ALIVE_PIC")
-    if a is not None:
+    pic_list = []
+    if a:
         b = a.split(" ")
-        c = []
         if len(b) >= 1:
-            for d in b:
-                c.append(d)
-        PIC = random.choice(c)
+            for c in b:
+                pic_list.append(c)
+        PIC = random.choice(pic_list)
     else:
         PIC = "https://telegra.ph/file/ea9e11f7c9db21c1b8d5e.mp4"
-    hell_pic = PIC
     end = datetime.datetime.now()
     ling = (end - start).microseconds / 1000
     omk = ALIVE_TEMP.format(ForGo10God, HELL_USER, tel_ver, hell_ver, is_sudo, uptime, ling)
-    await event.client.send_file(event.chat_id, file=hell_pic, caption=omk, parse_mode="HTML")
+    await event.client.send_file(event.chat_id, file=PIC, caption=omk, parse_mode="HTML")
     await hell.delete()
 
 
-msg = """{}\n
-<b><i>🏅 𝙱𝚘𝚝 𝚂𝚝𝚊𝚝𝚞𝚜 🏅</b></i>
-<b>Telethon ≈</b>  <i>{}</i>
-<b>Hêllẞø† ≈</b>  <i>{}</i>
-<b>Uptime ≈</b>  <i>{}</i>
-<b>Abuse ≈</b>  <i>{}</i>
-<b>Sudo ≈</b>  <i>{}</i>
-"""
-botname = Config.BOT_USERNAME
 
 @hell_cmd(pattern="hell$")
 async def hell_a(event):
@@ -66,7 +65,7 @@ async def hell_a(event):
     uptime = await get_time((time.time() - StartTime))
     am = gvarstat("ALIVE_MSG") or "<b>»» нєℓℓвσт ιѕ σиℓιиє ««</b>"
     try:
-        hell = await event.client.inline_query(botname, "alive")
+        hell = await event.client.inline_query(Config.BOT_USERNAME, "alive")
         await hell[0].click(event.chat_id)
         if event.sender_id == ForGo10God:
             await event.delete()
