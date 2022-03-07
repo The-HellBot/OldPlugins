0408517297707b74f1ac1e50db2363bfbadfd356
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 21:02:49 +0530

    Update convert.py

--- hellbot/plugins/convert.py
+++ hellbot/plugins/convert.py
@@ -27,7 +27,7 @@ async def _(event):
         quality = 512
     rply = await event.get_reply_message()
     hell_ = await event.client.download_media(rply.media)
-    gifs = tgs_to_gif(hell, quality)
+    gifs = tgs_to_gif(hell_, quality)
     await event.client.send_file(event.chat_id, file=gifs, force_document=False)
     await hell.delete()
 
