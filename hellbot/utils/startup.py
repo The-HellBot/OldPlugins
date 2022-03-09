112707e7d1a2f81528e089255125020fc73c8401
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-09 16:25:34 +0530

    Update startup.py

--- hellbot/utils/startup.py
+++ hellbot/utils/startup.py
@@ -60,7 +60,7 @@ async def start_msg(client, pic, version, total):
 
 <b><i>Version :</b></i> <code>{version}</code>
 <b><i>Clients :</b></i> <code>{total}</code>
-<b><i>Sudo :</b></i> <code>{sudo}</code>
+<b><i>Sudo :</b></i> <code>{is_sudo}</code>
 
 <b><i>»» <u><a href='https://t.me/Its_HellBot'>†hê Hêllẞø†</a></u> ««</i></b>
 """
