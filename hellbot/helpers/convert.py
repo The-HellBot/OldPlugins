ea0e8eec9596f081511051ed14a4db57357b0daf
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-09 12:34:25 +0530

    Update convert.py

--- hellbot/helpers/convert.py
+++ hellbot/helpers/convert.py
@@ -178,4 +178,13 @@ async def VSticker(event, file):
     return "VideoSticker.webm"
 
 
+# pass a string to convert it into integers
+async def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
+
+
 # hellbot
