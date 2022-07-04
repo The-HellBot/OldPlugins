b08b4a83fed10f960f502f0cba8714a178f29155
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-04 13:49:12 +0530

    Update mmf.py

--- TelethonHell/helpers/mmf.py
+++ TelethonHell/helpers/mmf.py
@@ -9,7 +9,7 @@ async def draw_meme_text(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "hellbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
+        "HellConfig/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
@@ -116,7 +116,7 @@ async def draw_meme(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "hellbot/resources/fonts/impact.ttf", int((70 / 640) * i_width)
+        "HellConfig/resources/fonts/impact.ttf", int((70 / 640) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
