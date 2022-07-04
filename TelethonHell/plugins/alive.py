34ddfbf59611a5abdc454e3288943e5ec457ba74
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-04 15:45:52 +0530

    Update alive.py

--- TelethonHell/plugins/alive.py
+++ TelethonHell/plugins/alive.py
@@ -5,7 +5,7 @@ import time
 from telethon.errors import ChatSendInlineForbiddenError as noin
 from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot
 
-from hellbot.sql.gvar_sql import gvarstat
+from TelethonHell.DB.gvar_sql import gvarstat
 
 from . import *
 
@@ -84,6 +84,8 @@ async def hell_a(event):
 
 CmdHelp("alive").add_command(
     "alive", None, "Shows the Default Alive Message"
-).add_command("hell", None, "Shows Inline Alive Menu with more details.").add_warning(
+).add_command(
+    "hell", None, "Shows Inline Alive Menu with more details."
+).add_warning(
     "✅ Harmless Module"
 ).add()
