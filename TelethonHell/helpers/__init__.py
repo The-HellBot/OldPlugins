a42e65d65e63c6384e86794b4e384c534891f401
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-09 22:38:41 +0530

    Update __init__.py

--- TelethonHell/helpers/__init__.py
+++ TelethonHell/helpers/__init__.py
@@ -4,6 +4,7 @@ from .exceptions import *
 from .formats import *
 from .gdriver import *
 from .google import *
+from .ig_helper import *
 from .image import *
 from .int_str import *
 from .mediatype import *
