dfcd79c63ee42d8709a1bc2d6fa4e6e426b327af
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-10 08:37:31 +0530

    Update ig_helper.py

--- TelethonHell/helpers/ig_helper.py
+++ TelethonHell/helpers/ig_helper.py
@@ -10,83 +10,71 @@ async def IGDL(event, url):
     dl_path = "./insta/dl"
     if not os.path.isdir(dl_path):
         os.makedirs(dl_path)
+    
+    caption, file = None, None
     type = url.split("/")[3]
 
+    try:
+        pk = InstaGram.media_pk_from_url(url)
+        info = InstaGram.media_info(pk).dict()
+    except Exception as e:
+        LOGS.info(str(e))
+        return file, caption
+
     if type == "p":
-        try:
-            pk = InstaGram.media_pk_from_url(url)
-        except Exception as e:
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
-
-        try:
-            info = InstaGram.media_info(pk).dict()
-        except Exception as e:
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
-
         if info['media_type'] == 8:
             try:
                 file = InstaGram.album_download(pk, folder=dl_path)
             except Exception as e:
-                os.remove(dl_path)
-                return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+                LOGS.info(str(e))
+                file = None
 
         elif info['media_type'] == 1:
             try:
                 file = InstaGram.photo_download(pk, folder=dl_path)
             except Exception as e:
-                os.remove(dl_path)
-                return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+                LOGS.info(str(e))
+                file = None
 
         elif info['media_type'] == 2 and info['product_type'] == "feed":
             try:
                 file = InstaGram.video_download(pk, folder=dl_path)
             except Exception as e:
-                os.remove(dl_path)
-                return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+                LOGS.info(str(e))
+                file = None
 
     elif type == "tv":
         try:
-            pk = InstaGram.media_pk_from_url(url)
-        except Exception as e:
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
-        try:
             file = InstaGram.igtv_download(pk, folder=dl_path)
         except Exception as e:
-            os.remove(dl_path)
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+            LOGS.info(str(e))
+            file = None
 
     elif type == "reel":
         try:
-            pk = InstaGram.media_pk_from_url(url)
-        except Exception as e:
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
-        try:
             file = InstaGram.clip_download(pk, folder=dl_path)
         except Exception as e:
-            os.remove(dl_path)
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+            LOGS.info(str(e))
+            file = None
     
     elif type == "stories":
         try:
-            pk = InstaGram.story_pk_from_url(url)
-        except Exception as e:
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
-        try:
             file = InstaGram.story_download(pk, folder=dl_path)
         except Exception as e:
-            os.remove(dl_path)
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+            LOGS.info(str(e))
+            file = None
 
     elif type == "s" and "story_media_id" in url:
         try:
-            pk = InstaGram.media_pk_from_url(url)
-        except Exception as e:
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
-        try:
             file = InstaGram.story_download(pk, folder=dl_path)
         except Exception as e:
-            os.remove(dl_path)
-            return await eod(hell, f"**ERROR !!** \n\n`{e}`")
+            LOGS.info(str(e))
+            file = None
+    
+    if info["caption_text"]:
+        caption = info["caption_text"]
+        
+    return file, caption
 
 
 async def get_flag(event):
