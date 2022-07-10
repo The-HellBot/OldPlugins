1acd39bc7659354928024dcb02ea6a8d9d95d813
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-10 11:56:38 +0530

    Update instaAPI.py

--- TelethonHell/clients/instaAPI.py
+++ TelethonHell/clients/instaAPI.py
@@ -18,6 +18,7 @@ async def InstaGram(event):
         #cl.challenge_code_handler = challenge_code
         try:
             cl.login(Config.IG_USERNAME, Config.IG_PASSWORD)
+            cl.challenge_code_handler = await challenge_code(Config.IG_USERNAME, Config.IG_PASSWORD)
         except ChallengeRequired:
             await event.edit(f"Need to configure instagram! Go to @{Config.BOT_USERNAME}'s dm and finish the process!")
             await challenge_code(cl, Config.IG_USERNAME, Config.IG_PASSWORD)
