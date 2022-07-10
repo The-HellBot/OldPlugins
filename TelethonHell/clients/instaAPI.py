5a3e67f79c5824aac06490001dc0c0f26a86ad2d
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-10 11:53:48 +0530

    Update instaAPI.py

--- TelethonHell/clients/instaAPI.py
+++ TelethonHell/clients/instaAPI.py
@@ -15,7 +15,7 @@ async def InstaGram(event):
         cl = Client()
         if settings:
             cl.load_settings(settings)
-        cl.challenge_code_handler = challenge_code
+        #cl.challenge_code_handler = challenge_code
         try:
             cl.login(Config.IG_USERNAME, Config.IG_PASSWORD)
         except ChallengeRequired:
