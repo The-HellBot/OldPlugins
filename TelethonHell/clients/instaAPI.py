40e1b42d8f9c4dac77eeff9ecbee4207c6d66ce7
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-12 02:02:38 +0530

    Update instaAPI.py

--- TelethonHell/clients/instaAPI.py
+++ TelethonHell/clients/instaAPI.py
@@ -12,14 +12,13 @@ settings = "insta/settings.json" if os.path.exists("insta/settings.json") else {
 async def InstaGram(event):
     if Config.IG_SESSION:
         cl = Client(settings)
-        cl.challenge_code_handler = await challenge_code()
         try:
             cl.login_by_sessionid(Config.IG_SESSION)
         except LoginRequired:
             return await InstaGram(event)
         except Exception as e:
             LOGS.info(f"ERROR: {e}")
-            return False
+            return None
         cl.dump_settings("insta/settings.json")
         return cl
     else:
