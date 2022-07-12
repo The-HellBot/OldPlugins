b94cb9dd958e5b2ec7fbb72df2ee6ac81b028041
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-12 12:54:45 +0530

    bawal

--- TelethonHell/plugins/instagram.py
+++ TelethonHell/plugins/instagram.py
@@ -1,11 +1,8 @@
 """
 TODO:
-1. instagram post downloader (reel/igtv/post) - .igdl -p <link>
-2. instagram story downloader - igdl -s <link>
-3. recent 10 feed - .igdl -feed
-4. hashtag's top 10 posts download - .igdl -ht <hashtag>
-5. upload video/pic in instagram - .igup -reel <reply>
-6. upload stories in instagram - .igup -s <reply>
+1. instagram story downloader - igdl -s <link>
+2. recent 10 feed - .igdl -feed
+3. hashtag's top 10 posts download - .igdl -ht <hashtag>
 """
 import os
 import re
@@ -19,7 +16,7 @@ async def download(event):
     _, _, hell_mention = await client_id(event)
     hell = await eor(event, "IG downloader in action...")
     
-    if flag.lower() in ["-p", "-s"]:
+    if flag.lower() in ["-post", "-story"]:
         result = re.search(insta_regex, url)
         if not result:
             return await eod(hell, "Need a instagram post/story link to download!")
@@ -47,7 +44,7 @@ async def download(event):
     elif flag.lower() == "-feed":
         await eod(hell, "soon")
         #### Recent 10 posts from feed
-    elif flag.lower() == "-ht":
+    elif flag.lower() == "-htag":
         await eod(hell, "soon")
         #### top 10 posts of hashtag
     else:
@@ -59,32 +56,96 @@ async def download(event):
     flag, url = await get_flag(event)
     hell = await eor(event, "IG uploader in action...")
     reply = await event.get_reply_message()
-    IG = await InstaGram(event)
+    caption = reply.message or "#Uploaded By HellBot"
+    HELL_MEDIA = media_type(reply)
+
+    try:
+        IG = await InstaGram(event)
+    except Exception as e:
+        return await eod(hell, f"**ERROR:** \n\n`{str(e)}`")
 
     if not reply:
         return await eod(hell, "Reply to a media to upload on instagram.")
     if not reply.media:
         return await eod(hell, "Reply to a media to upload on instagram.")
-    
-    HELL_MEDIA = media_type(reply)
-    if HELL_MEDIA not in ["Photo", "Gif", "Video"]:
-        return await eod(hell, "Reply to a media to upload on instagram.")
 
     if flag.lower() == "-reel":
-        caption = event.message.message or "#Uploaded By HellBot"
+        if HELL_MEDIA not in ["Gif", "Video"]:
+            return await eod(hell, "A reel can only be GIF or Video!")
         file = await event.client.download_media(reply)
-        await hell.edit("**Downloaded!** \n\nNow uploading to instagram...")
+        await hell.edit("**Downloaded!** \n\nNow uploading reel to instagram...")
         try:
-            video = IG.clip_upload(file, caption=caption)
+            video = IG.clip_upload(path=file, caption=caption)
         except Exception as e:
             os.remove(file)
             return await eod(hell, f"**ERROR !!** \n\n`{str(e)}`")
-        await hell.edit(f"**Uploaded to Instagram!** \n\n[See Post From Here](https://instagram.com/p/{video.code})", link_preview=False)
+        await hell.edit(f"**Uploaded Reel to Instagram!** \n\n[See Post From Here](https://instagram.com/p/{video.code})", link_preview=False)
+        os.remove(file)
+    
+    elif flag.lower() == "-tv":
+        if HELL_MEDIA not in ["Gif", "Video"]:
+            return await eod(hell, "An IGTV can only be GIF or Video!")
+        file = await event.client.download_media(reply)
+        await hell.edit("**Downloaded!** \n\nNow uploading IGTV to instagram...")
+        try:
+            video = IG.igtv_upload(path=file, title=caption, caption=caption)
+        except Exception as e:
+            os.remove(file)
+            return await eod(hell, f"**ERROR !!** \n\n`{str(e)}`")
+        await hell.edit(f"**Uploaded IGTV to Instagram!** \n\n[See Post From Here](https://instagram.com/p/{video.code})", link_preview=False)
+        os.remove(file)
+    
+    elif flag.lower() == "-vid":
+        if HELL_MEDIA not in ["Gif", "Video"]:
+            return await eod(hell, "A video post can only be GIF or Video!")
+        file = await event.client.download_media(reply)
+        await hell.edit("**Downloaded!** \n\nNow uploading Video to instagram...")
+        try:
+            video = IG.video_upload(path=file, caption=caption)
+        except Exception as e:
+            os.remove(file)
+            return await eod(hell, f"**ERROR !!** \n\n`{str(e)}`")
+        await hell.edit(f"**Uploaded Video to Instagram!** \n\n[See Post From Here](https://instagram.com/p/{video.code})", link_preview=False)
         os.remove(file)
 
-    elif flag.lower() == "-s":
-        await eod(hell, "soon")
-        #### instagram story upload from tg
+    elif flag.lower() == "-pic":
+        if HELL_MEDIA != "Photo":
+            return await eod(hell, "A picture post can only be a Photo!")
+        file = await event.client.download_media(reply)
+        await hell.edit("**Downloaded!** \n\nNow uploading Photo to instagram...")
+        try:
+            video = IG.photo_upload(path=file, caption=caption)
+        except Exception as e:
+            os.remove(file)
+            return await eod(hell, f"**ERROR !!** \n\n`{str(e)}`")
+        await hell.edit(f"**Uploaded Photo to Instagram!** \n\n[See Post From Here](https://instagram.com/p/{video.code})", link_preview=False)
+        os.remove(file)
+
+    elif flag.lower() == "-story":
+        if HELL_MEDIA in ["Gif", "Video"]:
+            file = await event.client.download_media(reply)
+            await hell.edit("**Downloaded!** \n\nNow uploading Story to instagram...")
+            try:
+                video = IG.video_upload_to_story(path=file, caption=caption)
+            except Exception as e:
+                os.remove(file)
+                return await eod(hell, f"**ERROR !!** \n\n`{str(e)}`")
+            await hell.edit(f"**Uploaded Story to Instagram!** \n\n[See Story From Here](https://instagram.com/p/{video.code})", link_preview=False)
+            os.remove(file)
+
+        elif HELL_MEDIA == "Photo":
+            file = await event.client.download_media(reply)
+            await hell.edit("**Downloaded!** \n\nNow uploading Story to instagram...")
+            try:
+                video = IG.photo_upload_to_story(path=file, caption=caption)
+            except Exception as e:
+                os.remove(file)
+                return await eod(hell, f"**ERROR !!** \n\n`{str(e)}`")
+            await hell.edit(f"**Uploaded Story to Instagram!** \n\n[See Story From Here](https://instagram.com/p/{video.code})", link_preview=False)
+            os.remove(file)
+        
+        else:
+            return await eod(hell, "Invalid media format. Only Videos, Pictures, GIF are supported to upload story.")
 
     else:
         await eod(hell, f"Give proper flag. Check `{hl}plinfo instagram` for details.")
@@ -92,11 +153,13 @@ async def download(event):
 
 
 CmdHelp("instagram").add_command(
-    "igdl", "<flag> <link>", "Instagram posts downloader based on flags."
+    "igdl", "<flag> <link>", "Download posts/reels/stories from Instagram. Requires INSTAGRAM_SESSION to work."
 ).add_command(
-    "igup", "<flag> <reply>", "Upload posts on Instagram from Telegram. Replied video/pic is uploaded with caption."
+    "igup", "<flag> <reply>", "Upload replied media on Instagram with caption from Telegram."
 ).add_extra(
-    "🚩 Flags", "-p, -s, -feed(igdl only), -ht(igdl only), -reel(igup only)"
+    "🚩 Flags [igdl]", "-post, -story, -feed, -htag"
+).add_extra(
+    "🚩 Flags [igup]", "-reel, -tv, -vid, -pic, -story"
 ).add_info(
     "Instagram API for Telegram."
 ).add_warning(
