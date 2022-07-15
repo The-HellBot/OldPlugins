01f2f140ed0d12ff2da5d8ed968eed8911c278d9
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-15 13:09:04 +0530

    Update edits.py

--- TelethonHell/plugins/edits.py
+++ TelethonHell/plugins/edits.py
@@ -1,10 +1,11 @@
 import asyncio
 import random
+import pyfiglet
 
 from . import *
 
 
-@hell_cmd(pattern="type ([\s\S]*)")
+@hell_cmd(pattern="type(?:\s|$)([\s\S]*)")
 async def _(event):
     input_str = event.pattern_match.group(1)
     shiiinabot = "\u2060"
@@ -33,7 +34,7 @@ async def _(event):
         await asyncio.sleep(0.3)
 
 
-@hell_cmd(pattern="emoji ([\s\S]*)")
+@hell_cmd(pattern="emoji(?:\s|$)([\s\S]*)")
 async def _(event):
     animation_interval = 0.3
     animation_ttl = range(0, 16)
@@ -116,6 +117,45 @@ async def metoo(e):
     await eor(e, txt)
 
 
+@hell_cmd(pattern="figlet(?:\s|$)([\s\S]*)")
+async def figlet(event):
+    CMD_FIG = {
+        "slant": "slant",
+        "3D": "3-d",
+        "5line": "5lineoblique",
+        "alpha": "alphabet",
+        "banner": "banner3-D",
+        "doh": "doh",
+        "iso": "isometric1",
+        "letter": "letters",
+        "allig": "alligator",
+        "dotm": "dotmatrix",
+        "bubble": "bubble",
+        "bulb": "bulbhead",
+        "digi": "digital",
+    }
+    input_str = event.pattern_match.group(1)
+    if ":" in input_str:
+        text, cmd = input_str.split(":", 1)
+    elif input_str is not None:
+        cmd = None
+        text = input_str
+    else:
+        await eod(event, "Please add some text to figlet")
+        return
+    if cmd is not None:
+        cmd = cmd.strip()
+        try:
+            font = CMD_FIG[cmd]
+        except KeyError:
+            await eod(event, "Invalid selected font.")
+            return
+        result = pyfiglet.figlet_format(text, font=font)
+    else:
+        result = pyfiglet.figlet_format(text)
+    await eor(event, "‌‌‎`{}`".format(result))
+
+
 CmdHelp("edits").add_command(
     "hehe", None, "Use and see"
 ).add_command(
@@ -140,6 +180,8 @@ CmdHelp("edits").add_command(
     "type", "<word>", "Animates the given word into a typewriter."
 ).add_command(
     "emoji", None, "Available cmnds are:-\n• shrug\n• apple\n• :/\n• -_-\n Add .emoji in front of all cmds."
+).add_command(
+    "figlet", "text : type", "The types are slant, 3D, 5line, alpha, banner, doh, iso, letter, allig, dotm, bubble, bulb, digi"
 ).add_info(
     "Bass Bakchodi hai ye."
 ).add_warning(
