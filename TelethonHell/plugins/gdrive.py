aaa9059ec969fba2d5d368a8026bea3e8d020f7a
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-15 13:37:22 +0530

    Update gdrive.py

--- TelethonHell/plugins/gdrive.py
+++ TelethonHell/plugins/gdrive.py
@@ -1,10 +1,13 @@
 from . import *
 
 
-@hell_cmd(pattern="gdl$")
+@hell_cmd(pattern="gdl(?:\s|$)([\s\S]*)")
 async def g_download(event):
     hell = await eor(event, "Accessing gdrive...")
-    drive_link = event.text[5:]
+    lists = event.text.split(" ", 2)
+    if len(lists) <= 1:
+        return await eod(hell, "Give a link to download")
+    drive_link = lists[1]
     await hell.edit(f"**Drive Link :** `{drive_link}`")
     file_id = await get_id(drive_link)
     await hell.edit("Downloading requested file from G-Drive...")
