95ca492f5a1118b00ceb36d6a66587d6688a6691
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-23 15:41:37 +0530

    Update progress.py

--- TelethonHell/helpers/progress.py
+++ TelethonHell/helpers/progress.py
@@ -32,20 +32,18 @@ async def progress(
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "[{0}{1}] {2}%\n".format(
-            "".join(["▰" for i in range(math.floor(percentage / 10))]),
-            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+        progress_str = "**[**{0}{1}**]** : __{2}%__\n".format(
+            "".join(["◙" for i in range(math.floor(percentage / 10))]),
+            "".join(["◘" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2),
         )
-        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+        tmp = progress_str + "__{0}__ **of** __{1}__\n**Time Remaining:** `{2}`".format(
             humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
         )
         if file_name:
-            await event.edit(
-                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
-            )
+            await event.edit(f"{type_of_ps}\n**• File Name:** `{file_name}` \n\n{tmp}")
         else:
-            await event.edit("{}\n{}".format(type_of_ps, tmp))
+            await event.edit(f"{type_of_ps} \n\n{tmp}")
 
 
 # gets output in readable format
