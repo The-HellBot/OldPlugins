814d3fe45cdc00b813b4976557febcd9fd029369
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-28 11:07:23 +0530

    Update runner.py

--- TelethonHell/helpers/runner.py
+++ TelethonHell/helpers/runner.py
@@ -56,4 +56,11 @@ async def env_safe_clean(text, harm):
 
     return output
 
+
+async def absolute_paths(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            yield os.path.abspath(os.path.join(dirpath, f))
+
+
 # hellbot
