b46f2856926966cfdd50c5eaa0bf8a1fc14bd7c4
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-28 11:06:18 +0530

    Update songs.py

--- TelethonHell/plugins/songs.py
+++ TelethonHell/plugins/songs.py
@@ -186,8 +186,32 @@ async def _(event):
 
 @hell_cmd(pattern="spotify(?:\s|$)([\s\S]*)")
 async def _(event):
-    await eor(event, "TO-DO")
-    # TODO
+    reply = await event.get_reply_message()
+    dirs = "./spotify/"
+    lists = event.text.split(" ", 1)
+    if not len(lists) == 2:
+        return await parse_error(event, "Nothing given to search on spotify.")
+    query = lists[1].strip()
+    hell = await eor(event, f"__Downloading__ `{query}` __from spotify ...__")
+    cmd = f"spotdl '{query}' --path-template 'spotify" + "/{artist}/{album}/{artist} - {title}.{ext}'"
+    await runcmd(cmd)
+    art_list = os.listdir(dirs)
+    dldirs = [i async for i in absolute_paths(dirs)]
+    if len(dldirs) == 0:
+        return await eod(hell, "Not found anything related to that.")
+    for music in dldirs:
+        try:
+            await event.client.send_file(
+                event.chat_id,
+                file=music,
+                caption=f"**✘ Spotify Song Downloaded !!** \n\n**« ✘ »** {hell_mention}",
+                reply_to=reply,
+                supports_streaming=True,
+            )
+            os.remove(music)
+        except Exception as e:
+            LOGS.info(str(e))
+    await hell.delete()
 
 
 CmdHelp("songs").add_command(
