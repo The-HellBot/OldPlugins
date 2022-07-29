d7f97b1f7f3bffeb336dde086c9e15d153459dd2
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-29 20:23:36 +0530

    Update telegraph.py

--- TelethonHell/plugins/telegraph.py
+++ TelethonHell/plugins/telegraph.py
@@ -1,30 +1,39 @@
 import datetime
 import os
+import random
+import string
 
 from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 
 from . import *
 
+telegraph = Telegraph()
+account = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
+auth_url = account["auth_url"]
+
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
+
 
 @hell_cmd(pattern="t(m|t)(?:\s|$)([\s\S]*)")
 async def _(event):
-    if Config.LOGGER_ID is None:
-        await eod(event, "You need to setup `LOGGER_ID` to use telegraph...")
-        return
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    telegraph = Telegraph()
-    optional_title = event.text[4:]
+    lists = event.text.split(" ", 1)
+    optional_title = None
+    if len(lists) == 2:
+        optional_title = lists[1].strip()
     hell = await eor(event, "Making Telegraph Link....")
     ForGo10God, HELL_USER, hell_mention = await client_id(event)
-    if event.reply_to_msg_id:
+    reply = await event.get_reply_message()
+    if reply:
         start = datetime.datetime.now()
-        r_message = await event.get_reply_message()
-        input_str = event.text[2:3]
+        input_str = lists[0][2:3]
         if input_str == "m":
             downloaded_file_name = await event.client.download_media(
-                r_message, Config.TMP_DOWNLOAD_DIRECTORY
+                reply, Config.TMP_DOWNLOAD_DIRECTORY
             )
             end = datetime.datetime.now()
             ms = (end - start).seconds
@@ -37,7 +46,7 @@ async def _(event):
                 start = datetime.datetime.now()
                 media_urls = upload_file(downloaded_file_name)
             except exceptions.TelegraphException as exc:
-                await eod(hell, "ERROR: " + str(exc), 8)
+                await parse_error(hell, exc)
                 os.remove(downloaded_file_name)
             else:
                 end = datetime.datetime.now()
@@ -53,17 +62,16 @@ async def _(event):
                     link_preview=True,
                 )
         elif input_str == "t":
-            user_object = await event.client.get_entity(r_message.sender_id)
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
-            # apparently, all Users do not have last_name field
+            user_object = await event.client.get_entity(reply.sender_id)
+            title_of_page = user_object.first_name
             if optional_title:
                 title_of_page = optional_title
-            page_content = r_message.message
-            if r_message.media:
+            page_content = reply.message
+            if reply.media:
                 if page_content != "":
                     title_of_page = page_content
                 downloaded_file_name = await event.client.download_media(
-                    r_message, Config.TMP_DOWNLOAD_DIRECTORY
+                    reply, Config.TMP_DOWNLOAD_DIRECTORY
                 )
                 m_list = None
                 with open(downloaded_file_name, "rb") as fd:
@@ -72,27 +80,54 @@ async def _(event):
                     page_content += m.decode("UTF-8") + "\n"
                 os.remove(downloaded_file_name)
             page_content = page_content.replace("\n", "<br>")
-            response = telegraph.create_page(title_of_page, html_content=page_content)
+            try:
+                response = telegraph.create_page(title_of_page, html_content=page_content)
+            except Exception as e:
+                title_of_page = "".join(
+                    random.choice(list(string.ascii_lowercase + string.ascii_uppercase))
+                    for _ in range(16)
+                )
+                response = telegraph.create_page(title_of_page, html_content=page_content)
             end = datetime.datetime.now()
             ms = (end - start).seconds
-            hellboy = f"https://telegra.ph/{response['path']}"
             await hell.edit(
-                f"✓ **[Pasted to telegraph]({hellboy})** \n✓ **Time Taken :-** `{ms}` secs\n✓** By :**  {hell_mention} \n✓  `{hellboy}`",
+                f"✓ **[Pasted to telegraph](https://telegra.ph/{response['path']})** \n✓ **Time Taken :-** `{ms}` secs\n✓** By :**  {hell_mention} \n✓  `https://telegra.ph/{response['path']}`",
                 link_preview=True,
             )
     else:
         await eod(hell, "Reply to a message to get a permanent telegra.ph link.")
 
 
-def resize_image(image):
-    im = Image.open(image)
-    im.save(image, "PNG")
+@hell_cmd(pattern="tgraph(?:\s|$)([\s\S]*)")
+async def _(event):
+    hell = await eor(event, "Making telegraph post ...")
+    reply = await event.get_reply_message()
+    lists = event.text.split(" ", 1)
+    if not reply:
+        return await parse_error(event, "Nothing given to paste on telegraph.")
+    if not len(lists) == 2:
+        return await parse_error(event, "Give a title for the telegraph page!")
+    query = lists[1].split("|", 2)
+    title = None
+    auth = "[ †he Hêllẞø† ]"
+    url = "https://t.me/its_hellbot"
+    content = reply.message
+    if len(query) == 3:
+        title = query[0].strip()
+        auth = query[1].strip()
+        url = query[2].strip()
+    else:
+        title = query[0].strip()
+    link = await telegraph_paste(title, content, auth, url)
+    await hell.edit(f"**Created telegraph post!** \n\n__◈ Title:__ `{title}` \n__◈ Author:__ [{auth}]({url}) \n__◈ Link:__ {link}", link_preview=False)
 
 
 CmdHelp("telegraph").add_command(
     "tt", "<reply to text message>", "Uploads the replied text message to telegraph making a short telegraph link"
 ).add_command(
     "tm", "<reply to media>", "Uploads the replied media (sticker/ gif/ video/ image) to telegraph and gives a short telegraph link"
+).add_command(
+    "tgraph", "<reply to a text> <title|author|authorlink>", "Makes a telegraph page of replied content using html parser."
 ).add_info(
     "Make Telegraph Links."
 ).add_warning(
