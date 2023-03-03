3679e76fb5c999d64320f700c5ef86a24c809549
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2023-03-03 18:48:26 +0530

    Update client_list.py

--- TelethonHell/clients/client_list.py
+++ TelethonHell/clients/client_list.py
@@ -48,8 +48,8 @@ async def clients_list():
 async def client_id(event, botid=None):
     if botid is not None:
         uid = await event.client(GetFullUserRequest(botid))
-        ForGo10God = uid.user.id
-        HELL_USER = uid.user.first_name
+        ForGo10God = uid.user[0].id
+        HELL_USER = uid.user[0].first_name
         hell_mention = f"[{HELL_USER}](tg://user?id={ForGo10God})"
     else:
         client = await event.client.get_me()
