489a485c1032b4f916b797d3f7da41501e103d94
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2023-03-14 17:05:06 +0530

    update TelethonHell/plugins/adultzone.py

--- TelethonHell/plugins/adultzone.py
+++ TelethonHell/plugins/adultzone.py
@@ -1,6 +1,7 @@
 import random
 
 import nekos
+from WaifuPicsPython import WaifuAsync
 from TelethonHell.plugins import *
 
 neko_category = [
@@ -26,6 +27,46 @@ neko_category = [
     'goose',
     'woof',
 ]
+
+nsfw_list = ["waifu", "neko", "trap", "blowjob", "random"]
+sfw_list = ['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 'glomp', 'slap', 'kill', 'kick', 'happy', 'wink', 'poke', 'dance', 'cringe', 'random']
+
+@hell_cmd(pattern="nsfw(?:\s|$)([\s\S]*)")
+async def nsfw(event):
+    x = await event.get_chat()
+    y = x.id
+    if y = 1496036895:
+        return await parse_error(event, "Can't use this command here.")
+        if abuse_m == "Disabled":
+            return await eod(event, "**This command is only for users with variable** `ABUSE` **as** `ON`")
+    lists = event.text.split(" ")
+    category = None
+    limit = 0
+    if len(lists) == 1:
+        category = "random"
+        limit = 1
+    elif len(lists) == 2 & lists[1].isdigit():
+        category = "random"
+        limit = lists[1].strip()
+    elif len(lists) == 3:
+        category = lists[1].strip()
+        limit = lists[2].strip()
+    else:
+        category = lists[1].strip()
+        limit = 1
+    if int(limit) > 30:
+        limit = 30
+    if category not in nsfw_list:
+        nsfw_cat = "Available categories are: \n• `random` \n"
+        for n in nsfw_list:
+            nsfw_cat += f"• `{n}` \n"
+        txt = f"**Invalid category provided:** `{category}`\n\n{nsfw_cat}"
+        return await eor(event, txt)
+    waifu = WaifuAsync()
+    pics = await waifu.nsfw(category, many=True)
+    for p in range(int(limit)):
+        await event.client.send_file(event.chat_id, file=pics[p])
+    await event.delete()
 
 
 @hell_cmd(pattern="nekos(?:\s|$)([\s\S]*)")
