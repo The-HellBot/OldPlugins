363337c6f7694caa0d66b6419a0f80912dc34511
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2023-03-14 13:33:50 +0530

    Update songs.py

--- TelethonHell/plugins/songs.py
+++ TelethonHell/plugins/songs.py
@@ -40,6 +40,7 @@ async def songs(event):
         with yt_dlp.YoutubeDL(song_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
         c_time = time.time()
         await event.client.send_file(
             event.chat_id,
